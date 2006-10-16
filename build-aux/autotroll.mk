a3e180c49bb1dff8fb53060daf7cbd76fe828f41
(no author) <(no author)@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-10-16 17:56:51 +0000

    r40: Add AutoTroll.
    	* gui: New.
    	* gui/Makefile.am: New. Demo of AutoTroll.
    	* gui/lcdrange.cpp: New.
    	* gui/lcdrange.h: New.
    	* gui/main.cc: New.
    	* gui/qt_main.cpp: New.
    
    	* build-aux/autotroll.mk: New.
    	* build-aux/autotroll.m4: New.
    	* tests/test.sh: Enhance self-documentation.
    	* configure.ac: Don't look for a C compiler by default. Use AutoTroll.
    	* Makefile.am (ACLOCAL_AMFLAGS): New.
              (SUBDIRS): Add gui/.
    
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@42 f9a80dcf-8710-0410-9714-9a4fb6817a1c

---
+++ build-aux/autotroll.mk
@@ -0,0 +1,31 @@
+# Makerules.
+# This file is part of AutoTroll.
+# Copyright (C) 2006  Benoit Sigoure.
+#
+# AutoTroll is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+# USA.
+
+ # ------------- #
+ # DOCUMENTATION #
+ # ------------- #
+
+# See autotroll.m4 :)
+SUFFIXES = .moc.cpp
+.h.moc.cpp:
+	$(MOC) $(QT_CPPFLAGS) $< -o $@
+
+# FIXME: Add support for UIC?
+
+DISTCLEANFILES = $(BUILT_SOURCES)
