50c619acca11a9eeae7cee451c1d87e09161bccf
(no author) <(no author)@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-10-17 12:09:31 +0000

    Add libport. 	* libport: New. 	* build-aux/svn-externals: Know it. 	* build-aux/build-aux.mk: Know it.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@48 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/build-aux.mk
+++ build-aux/build-aux.mk
@@ -1,3 +1,7 @@
+## ----------- ##
+## Build-aux.  ##
+## ----------- ##
+
 .PHONY: baux-up baux-ci baux-pin
 
 # Update a pinned svn:externals.
@@ -12,3 +16,23 @@ baux-ci:
 # Pin the svn:external by subscribing to it.
 baux-pin:
 	cd $(top_srcdir) && build-aux/svn-externals --subscribe=baux
+
+
+## --------- ##
+## libport.  ##
+## --------- ##
+
+.PHONY: libport-up libport-ci libport-pin
+
+# Update a pinned svn:externals.
+libport-up:
+	cd $(top_srcdir) && build-aux/svn-externals --update=libport
+
+# Check in a pinned external from a host project, and depend upon it.
+libport-ci:
+	cd $(top_srcdir)/build-aux && vcs-svn ci
+	$(MAKE) $(AM_MAKEFLAGS) libport-up
+
+# Pin the svn:external by subscribing to it.
+libport-pin:
+	cd $(top_srcdir) && build-aux/svn-externals --subscribe=libport
