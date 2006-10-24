d8d2786b9429e0eafd78c293eebfab763a297cf9
(no author) <(no author)@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-10-24 06:49:24 +0000

    Installable libport 	* libport.mk: New. 	* README: Adjust.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@82 f9a80dcf-8710-0410-9714-9a4fb6817a1c

---
+++ libport/libport.mk
@@ -0,0 +1,14 @@
+# We install it in includedir, that easier currently.  Some
+# day everybody should move into includedir/urbi.
+
+# Where we find them in this package.
+libport_srcdir = $(top_srcdir)/libport
+# Where we install them.
+libportdir = $(includedir)/libport
+
+libport_HEADERS = 				\
+$(libport_srcdir)/hash.hh			\
+$(libport_srcdir)/lockable.hh			\
+$(libport_srcdir)/semaphore.hh			\
+$(libport_srcdir)/thread.hh			\
+$(libport_srcdir)/utime.hh
