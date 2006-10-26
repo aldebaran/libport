f19966f77277c90a1e6de46076f819d82d1ba873
(no author) <(no author)@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-10-26 13:09:39 +0000

    Add a sample test to show how to use the test-suite. 	* tests/testme.stdout: New. 	* tests/testme.cc: New. 	* tests/Makefile.am: Add a sample test. 	* Makefile.am (EXTRA_DIST): Complete.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@100 f9a80dcf-8710-0410-9714-9a4fb6817a1c

---
+++ tests/testme.cc
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Gostai S.A.S., 2006.
+ *
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
+ *
+ * See the LICENSE file for more information.
+ * For comments, bug reports and feedback: http://www.urbiforge.com
+ */
+
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Whatever" << std::endl;
+}
