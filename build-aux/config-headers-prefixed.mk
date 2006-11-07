c534ebd31626bc79a71df7669d3d9d6fe99709d4
(no author) <(no author)@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-11-07 12:41:00 +0000

    Simplify 	* config-headers-prefixed.mk: No need for a stamp file.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@152 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/config-headers-prefixed.mk
+++ build-aux/config-headers-prefixed.mk
@@ -1,17 +1,11 @@
-# Before including this makefile snippet, define:
-
+# There is no point in maintaining a stamp file, Automake already does
+# this for config.h.  Arguably, we could use _configs.sed, but it is
+# not documented.
+BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER)
 $(PREFIXED_CONFIG_HEADER): $(CONFIG_HEADER)
-	@if test ! -f $@; then \
-	  rm -f $(PREFIXED_CONFIG_HEADER).stamp; \
-	  $(MAKE) $(AM_MAKEFLAGS) $(PREFIXED_CONFIG_HEADER).stamp; \
-	else :; fi
-
-$(PREFIX_CONFIG_H).stamp: $(CONFIG_HEADER) $(top_builddir)/config.status
-	@rm -f $@
-	cd $(top_builddir) && $(SHELL) ./config.status $(PREFIXED_CONFIG_HEADER)
-	@touch $@
+	cd $(top_builddir) && \
+	$(SHELL) ./config.status $(PREFIXED_CONFIG_HEADER)
 
 CLEANFILES +=					\
 _configs.sed					\
-$(PREFIXED_CONFIG_HEADER) 			\
-$(PREFIXED_CONFIG_HEADER).stamp
+$(PREFIXED_CONFIG_HEADER)
