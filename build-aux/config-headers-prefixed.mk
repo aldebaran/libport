5beda088656798aad145fe0e5751c922dd49eead
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-11-15 11:56:59 +0000

    Fix handling of libport/config.h 	* urbi-config-headers-prefixed.m4 (PREFIXED_CONFIG_HEADER_basename): 	Define it. 	* config-headers-prefixed.mk: Use it.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@188 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/config-headers-prefixed.mk
+++ build-aux/config-headers-prefixed.mk
@@ -1,11 +1,11 @@
 # There is no point in maintaining a stamp file, Automake already does
 # this for config.h.  Arguably, we could use _configs.sed, but it is
 # not documented.
-BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER)
-$(PREFIXED_CONFIG_HEADER): $(CONFIG_HEADER)
+BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER_basename)
+$(PREFIXED_CONFIG_HEADER_basename): $(CONFIG_HEADER)
 	cd $(top_builddir) && \
 	$(SHELL) ./config.status $(PREFIXED_CONFIG_HEADER)
 
 CLEANFILES +=					\
 _configs.sed					\
-$(PREFIXED_CONFIG_HEADER)
+$(PREFIXED_CONFIG_HEADER_basename)
