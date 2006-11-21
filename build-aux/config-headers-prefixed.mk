f19d0fb7daeb6c6392cb3548d21a16f1723dabe3
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-11-21 13:15:40 +0000

    Simplify the creation of libport/config.h 	* config-headers-prefixed.mk: No longer try to work from 	another Makefile.am that the top-level's one. 	* urbi-config-headers-prefixed.m4: Simplify accordingly. 	* test.sh.in: Formatting changes.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@214 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/config-headers-prefixed.mk
+++ build-aux/config-headers-prefixed.mk
@@ -1,11 +1,18 @@
+# Use this file only from the top-level Makefile, otherwise
+# it won't produce the expect result.
+
+BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER)
+
 # There is no point in maintaining a stamp file, Automake already does
 # this for config.h.  Arguably, we could use _configs.sed, but it is
 # not documented.
-BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER_basename)
-$(PREFIXED_CONFIG_HEADER_basename): $(CONFIG_HEADER)
-	cd $(top_builddir) && \
+
+$(PREFIXED_CONFIG_HEADER): $(CONFIG_HEADER)
+# This does not work properly when used from another directory, so
+# let's make sure it dies if the directory is not the top level.
+#	cd $(top_builddir) &&
 	$(SHELL) ./config.status $(PREFIXED_CONFIG_HEADER)
 
 CLEANFILES +=					\
 _configs.sed					\
-$(PREFIXED_CONFIG_HEADER_basename)
+$(PREFIXED_CONFIG_HEADER)
