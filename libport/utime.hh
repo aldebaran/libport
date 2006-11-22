95ec18fd87e5dd8c4d9ce0efc1422a52ade81726
sigoure <sigoure@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-11-22 13:36:39 +0000

    Don't use LARGE_INTEGER. Use __int64 instead.         See:         http://msdn.microsoft.com/library/default.asp          `-> ?url=/library/en-us/winprog/winprog/large_integer_str.asp 	This works for sure on mingw/cygwin and MSVC++ which is enough for us. 	* utime.hh: Here.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@233 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/utime.hh
+++ libport/utime.hh
@@ -15,17 +15,19 @@ namespace urbi
   long long
   utime()
   {
-    static LARGE_INTEGER pfreq = 0;
-    static LARGE_INTEGER base = 0;
+    // NOTE: casting an __int64 in a LARGE_INTEGER is "safe" according to MSDN.
+    static __int64 pfreq = 0;
+    static __int64 base = 0;
+
     if (pfreq == 0)
     {
-      QueryPerformanceFrequency(&pfreq);
-      QueryPerformanceCounter(&base);
-      std::cerr <<"perfcounter at frequency of " << pfreq << "\n";
+      QueryPerformanceFrequency((LARGE_INTEGER*) &pfreq);
+      QueryPerformanceCounter ((LARGE_INTEGER*) &base);
+      std::cerr << "perfcounter at frequency of " << pfreq << "\n";
     }
-    LARGE_INTEGER val;
-    QueryPerformanceCounter(&val);
-    return (long long) ((val-base) * 1000000LL) / pfreq;
+    __int64 val;
+    QueryPerformanceCounter((LARGE_INTEGER*) &val);
+    return (long long) ((val - base) * 1000000LL) / pfreq;
   }
 }
 
@@ -41,12 +43,12 @@ namespace urbi
   utime()
   {
     static long long start = 0;
-    static const long long BIGDELTA = 30LL*365LL*24LL*3600LL;
+    static const long long BIGDELTA = 30LL * 365LL * 24LL * 3600LL;
     struct timeval tv;
     gettimeofday(&tv, 0);
     if (start == 0)
-      start = (tv.tv_sec-BIGDELTA)*1000000+tv.tv_usec;
-    return (tv.tv_sec-BIGDELTA)*1000000+tv.tv_usec - start;
+      start = (tv.tv_sec - BIGDELTA) * 1000000LL + tv.tv_usec;
+    return (tv.tv_sec - BIGDELTA) * 1000000LL + tv.tv_usec - start;
   }
 }
 
