c1cf5cafb1be4cfcf26bab7bed290ba8162df424
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-12-19 17:01:53 +0000

    Fix fwd.hh 	* fwd.hh: Adjust namespaces.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@311 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/fwd.hh
+++ libport/fwd.hh
@@ -1,20 +1,22 @@
 #ifndef LIBPORT_FWD_HH
 # define LIBPORT_FWD_HH
 
-namespace urbi
+namespace libport
 {
   // hash.hh.
   template<class K, class V>
   class hash_map_type;
 
+  // ref-pt.hh
+  template <class T> class RefPt;
+}
+
+namespace urbi
+{
   // lockable.hh.
   class Lockable;
 
   // semaphore.hh.
   class Semaphore;
-
-  // refpt.hh
-  template <class T> class RefPt;
 }
-
 #endif // ! LIBPORT_FWD_HH
