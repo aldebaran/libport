3b8ffd444b1aaa46c29b41c286fe6c5f05cb2be6
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2006-12-29 10:59:14 +0000

    Upgrade check.mk 	* check.mk (am__rst_section, am__rst_subsection) 	(am__text_box, am__check_pre, am__check_post): New. 	Use the latter two to factor the log production rules. 	Use the former ones to improve the Rst output of the log.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@345 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/check.mk
+++ build-aux/check.mk
@@ -31,65 +31,67 @@
 ## We use GNU Make extensions, and override check-TESTS.
 AUTOMAKE_OPTIONS += -Wno-portability -Wno-override
 
+# Restructured Text section and subsection.
+am__rst_section =    sed 'p;s/./=/g'
+am__rst_subsection = sed 'p;s/./-/g'
+
+# Put in a box.
+am__text_box = $(AWK) '{gsub ("\\.  ", "\n"); print $$0; }' |	\
+$(AWK) '							\
+max < length($$0) {						\
+  final= final (final ? "\n" : "") " " $$0;			\
+  max = length($$0);						\
+}								\
+END { 								\
+  for (i = 0; i < max + 2 ; ++i)				\
+    line = line "=";						\
+  print line;							\
+  print final;							\
+  print line;							\
+}'
+
+# To be inserted before the command running the test.  Stores in $dir
+# the directory containing $<, and passes the TEST_ENVIRONMENT.
+am__check_pre =					\
+if test -f ./$<; then dir=./;			\
+elif test -f $<; then dir=;			\
+else dir="$(srcdir)/"; fi;			\
+$(TESTS_ENVIRONMENT)
+
+# To be appended to the command running the test.  Handles the stdout
+# and stderr redirection, and catch the exit status.
+am__check_post=						\
+>$@-t 2>&1;						\
+status=$$?;						\
+case $$status:" $(XFAIL_TESTS) " in			\
+    0:*' $< '*) res='XPASS';;				\
+    0:*)        res='PASS' ;;				\
+    77:*)       res='SKIP' ;;				\
+    *:*' $< '*) res='XFAIL';;				\
+    *:*)        res='FAIL' ;;				\
+   esac;						\
+echo "$$res: $$(basename $<)";				\
+echo "$$res: $$(basename $<) (exit: $$status)" |	\
+  $(am__rst_subsection) >$@;				\
+cat $@-t >>$@;						\
+rm $@-t
+
 SUFFIXES = .log .test
 # From a test file to a log file.
 # Do not use a regular `.test.log:' rule here, since in that case the
 # following rule (without incoming extension) will mask this one.
 %.log: %.test
-	@if test -f ./$<; then dir=./;			\
-	elif test -f $<; then dir=;			\
-	else dir="$(srcdir)/"; fi;			\
-	$(TESTS_ENVIRONMENT) $${dir}$< >$@-t 2>&1;	\
-	status=$$?;					\
-	case $$status:" $(XFAIL_TESTS) " in		\
-	    0:*' $< '*) res='XPASS';;			\
-	    0:*)        res='PASS' ;;			\
-	    77:*)       res='SKIP' ;;			\
-	    *:*' $< '*) res='XFAIL';;			\
-	    *:*)        res='FAIL' ;;			\
-	   esac;					\
-	echo "$$res: $$(basename $<)";			\
-	echo "$$res: $$(basename $<) (exit: $$status)" >$@
-	@cat $@-t >>$@
-	@rm $@-t
+	@$(am__check_pre) $${dir}$< $(am__check_post)
 
 # The exact same commands, but for programs without extensions.
 %.log: %
-	@if test -f ./$<; then dir=./;			\
-	elif test -f $<; then dir=;			\
-	else dir="$(srcdir)/"; fi;			\
-	$(TESTS_ENVIRONMENT) $${dir}$< >$@-t 2>&1;	\
-	status=$$?;					\
-	case $$status:" $(XFAIL_TESTS) " in		\
-	    0:*' $< '*) res='XPASS';;			\
-	    0:*)        res='PASS' ;;			\
-	    77:*)       res='SKIP' ;;			\
-	    *:*' $< '*) res='XFAIL';;			\
-	    *:*)        res='FAIL' ;;			\
-	   esac;					\
-	echo "$$res: $$(basename $<)";			\
-	echo "$$res: $$(basename $<) (exit: $$status)" >$@
-	@cat $@-t >>$@
-	@rm $@-t
+	@$(am__check_pre) $${dir}$< $(am__check_post)
 
 TEST_LOGS ?= $(TESTS:.test=.log)
 TEST_SUITE_LOG = test-suite.log
 
 $(TEST_SUITE_LOG): $(TEST_LOGS)
-	@{								\
-	  echo "$(subdir)/$(TEST_SUITE_LOG)" | sed -e 'p;s/./=/g';	\
-	  echo;								\
-	  echo ".. contents:: :depth: 2";				\
-	  echo;								\
-	  for f in $(TEST_LOGS);					\
-	  do								\
-	    case $$(sed 1q $$f) in					\
-	      SKIP:*|PASS:*|XFAIL:*);;					\
-	      *) cat $$f;						\
-	    esac;							\
-	  done;								\
-	} >$(TEST_SUITE_LOG);						\
-	results=$$(for f in $(TEST_LOGS); do sed 1q $$f; done);		\
+	@results=$$(for f in $(TEST_LOGS); do sed 1q $$f; done);	\
 	all=$$(echo "$$results" | wc -l | sed -e 's/^[ \t]*//');	\
 	fail=$$(echo "$$results" | grep -c '^FAIL');			\
 	pass=$$(echo "$$results" | grep -c '^PASS');			\
@@ -98,30 +100,44 @@ $(TEST_SUITE_LOG): $(TEST_LOGS)
 	xpass=$$(echo "$$results" | grep -c '^XPASS');			\
 	case fail=$$fail:xfail=$$xfail:xpass=$$xpass in			\
 	  fail=0:xfail=0:xpass=*)					\
-	    msg="All $$all tests passed";;				\
+	    msg="All $$all tests passed.  ";;				\
 	  fail=0:xfail=*:xpass=*)					\
 	    msg="All $$all tests behaved as expected";			\
-	    msg="$$msg ($$xfail expected failures)";;			\
+	    msg="$$msg ($$xfail expected failures).  ";;		\
 	  fail=*:xfail=*:xpass=0)					\
-	    msg="$$fail of $$all tests failed";;			\
+	    msg="$$fail of $$all tests failed.  ";;			\
 	  fail=*:xfail=*:xpass=*)					\
 	    msg="$$fail of $$all tests did not behave as expected";	\
-	    msg="$$msg ($$xpass unexpected passes)";;			\
+	    msg="$$msg ($$xpass unexpected passes).  ";;		\
 	  *)								\
             echo >&2 "incorrect case"; exit 4;;				\
 	esac;								\
+	if test "$$skip" -ne 0; then					\
+	  msg="$$msg($$skip tests were not run).  ";			\
+	fi;								\
 	{								\
+	  echo "$(PACKAGE_STRING): $(subdir)/$(TEST_SUITE_LOG)" |	\
+	    $(am__rst_section);						\
+	  echo;								\
 	  echo "$$msg";							\
-	  if test "$$skip" -ne 0; then					\
-	    echo "($$skip tests were not run)";				\
+	  echo;								\
+	  echo ".. contents:: :depth: 2";				\
+	  echo;								\
+	  for f in $(TEST_LOGS);					\
+	  do								\
+	    case $$(sed 1q $$f) in					\
+	      SKIP:*|PASS:*|XFAIL:*);;					\
+	      *) cat $$f;;						\
+	    esac;							\
+	  done;								\
+	} >$(TEST_SUITE_LOG);						\
+	if test "$$fail" -ne 0; then					\
+	  msg="$${msg}See $(subdir)/$(TEST_SUITE_LOG).  ";		\
+	  if test -n "$(PACKAGE_BUGREPORT)"; then			\
+	    msg="$${msg}Please report it to $(PACKAGE_BUGREPORT).  ";	\
 	  fi;								\
-	  if test "$$fail" -ne 0; then					\
-	    echo "See $(subdir)/$(TEST_SUITE_LOG)";			\
-	    if test -n "$(PACKAGE_BUGREPORT)"; then			\
-	      echo "Please report it to $(PACKAGE_BUGREPORT)";		\
-	    fi;								\
-	  fi;								\
-	} | sed -e '1h;1s/./=/g;1p;1x;$$p;$$x';				\
+	fi;								\
+	echo "$$msg" | $(am__text_box);					\
 	if test x"$$VERBOSE" != x; then					\
 	  cat $(TEST_SUITE_LOG);					\
 	fi;								\
