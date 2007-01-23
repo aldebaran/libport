c76b5f7aac2a23a38ccd1cd0b6a815195ce06d71
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-01-23 02:39:20 +0000

    LIBPORT_FD_SET 	* libport/network.h: Define it here.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@434 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/network.h
+++ libport/network.h
@@ -29,4 +29,15 @@ typedef int socklen_t;
 
 # endif /* WIN32 */
 
+// In the long run, this should be part of libport, but I don't know
+// where actually :( Should it be libport/sys/types.hh?  Or unistd.hh?
+// What standard header should do that?
+
+#ifdef WIN32
+// On windows, file descriptors are defined as u_int (i.e., unsigned int).
+# define LIBPORT_FD_SET(N, P) FD_SET(static_cast<u_int>(N), P)
+#else
+# define LIBPORT_FD_SET(N, P) FD_SET(N, P)
+#endif
+
 #endif /* !LIBPORT_NETWORK_H_ */
