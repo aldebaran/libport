4e1832c05936b9c51cefee5dfcbc9d558437984f
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-01-23 02:55:20 +0000

    Comments removal 	* network.h: Remove obsolete comments.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@435 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/network.h
+++ libport/network.h
@@ -29,9 +29,6 @@ typedef int socklen_t;
 
 # endif /* WIN32 */
 
-// In the long run, this should be part of libport, but I don't know
-// where actually :( Should it be libport/sys/types.hh?  Or unistd.hh?
-// What standard header should do that?
 
 #ifdef WIN32
 // On windows, file descriptors are defined as u_int (i.e., unsigned int).
