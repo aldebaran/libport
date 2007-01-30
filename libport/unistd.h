d00a35525d432d83c098519b40939e4095b058e7
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-01-30 09:20:20 +0000

    unistd.h -> io.h 	* unistd.h: Include io.h on Windows.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@472 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/unistd.h
+++ libport/unistd.h
@@ -1,8 +1,17 @@
 #ifndef LIBPORT_UNISTD_H_
 # define LIBPORT_UNISTD_H_
 
+# include "detect_win32.h"
+
+// This is traditional Unix file.
 # ifdef HAVE_UNISTD_H
 #  include "unistd.h"
+# endif
+
+// This seems to be its WIN32 equivalent.
+// http://msdn2.microsoft.com/en-us/library/ms811896(d=printer).aspx#ucmgch09_topic7.
+# if defined WIN32 || defined LIBPORT_WIN32
+#  include "io.h"
 # endif
 
 #endif /* !LIBPORT_UNISTD_H_ */
