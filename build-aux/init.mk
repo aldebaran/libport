6a10b01d4e310acd2f78fd5d18b47c30dc04cc47
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-02-02 09:14:01 +0000

    Clean manifest files. 	* init.mk (CLEANFILES): Add *.exe.manifest.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@516 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/init.mk
+++ build-aux/init.mk
@@ -7,6 +7,8 @@ BUILT_SOURCES =
 # Visual Studio Debugger PDB Files (C++).
 # http://msdn2.microsoft.com/en-us/library/yd4f8bd1.aspx.
 CLEANFILES = vc80.pdb
+# Files created by MSC++.
+CLEANFILES += *.exe.manifest
 EXTRA_DIST =
 EXTRA_HEADERS =
 MAINTAINERCLEANFILES =
