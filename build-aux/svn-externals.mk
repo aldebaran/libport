0f49db85efdba273a0e969d278d87e9337b181a7
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-02-12 12:17:47 +0000

    * svn-externals.mk (<external>-upci): New.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@557 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/svn-externals.mk
+++ build-aux/svn-externals.mk
@@ -1,4 +1,4 @@
-# To use this file, accumulate in 
+# To use this file, accumulate in
 #
 # - SVN_EXTERNALS
 #   the list of directories names that are svn:externals.
@@ -26,19 +26,18 @@ SVN_EXTERNALS_CI   = $(SVN_EXTERNALS:=-ci)
 SVN_EXTERNALS_HELP = $(SVN_EXTERNALS:=-help)
 SVN_EXTERNALS_PIN  = $(SVN_EXTERNALS:=-pin)
 SVN_EXTERNALS_UP   = $(SVN_EXTERNALS:=-up)
+SVN_EXTERNALS_UPCI = $(SVN_EXTERNALS:=-upci)
 
 .PHONY: $(SVN_EXTERNALS_HELP) $(SVN_EXTERNALS_PIN) \
-        $(SVN_EXTERNALS_CI)   $(SVN_EXTERNALS_UP)
+        $(SVN_EXTERNALS_CI)   $(SVN_EXTERNALS_UP)  \
+        $(SVN_EXTERNALS_UPCI)
 help: $(SVN_EXTERNALS_HELP)
 $(SVN_EXTERNALS_HELP):
-	@ext=$(@:-help=);						 \
-	echo "$$ext-up:    update the svn:externals revision for $$ext"; \
-	echo "$$ext-ci:    check in $$ext and up it";			 \
+	@ext=$(@:-help=);							\
+	echo "$$ext-ci:    check in $$ext and up it";				\
+	echo "$$ext-up:    update the svn:externals revision for $$ext";	\
+	echo "$$ext-upci:  up $$ext and and check in its host directory";	\
 	echo "$$ext-pin:   subscribe to the latest $$ext"
-
-# Update the pinned external.
-$(SVN_EXTERNALS_UP):
-	$(svn_externals) --update=$(@:-up=) $(srcdir)
 
 # Checkin the pinned external and update it.
 $(SVN_EXTERNALS_CI):
@@ -48,6 +47,15 @@ $(SVN_EXTERNALS_CI):
 # Pin the svn:external by subscribing to it.
 $(SVN_EXTERNALS_PIN):
 	$(svn_externals) --subscribe=$(@:-pin=) $(srcdir)
+
+# Update the pinned external.
+$(SVN_EXTERNALS_UP):
+	$(svn_externals) --update=$(@:-up=) $(srcdir)
+
+# Update the pinned external.
+$(SVN_EXTERNALS_UPCI):
+	$(MAKE) $(AM_MAKEFLAGS) $(@:-upci=-up)
+	svn ci -m "Update $(@:-upci=)." -N $(srcdir)
 
 
 ## ------------------- ##
@@ -61,6 +69,8 @@ externals-help:
 	@echo "externals-ci:    check in all the svn:externals dependencies"
 	@echo "externals-up:    update all the svn:externals dependencies"
 
-# Externals at once.
+# All the externals at once.
 externals-ci: $(SVN_EXTERNALS_CI) $(SVN_EXTERNALS_PROXY:=-ci)
 externals-up: $(SVN_EXTERNALS_UP) $(SVN_EXTERNALS_PROXY:=-up)
+externals-upci: $(SVN_EXTERNALS_UP)
+	svn ci -m "Update externals." $(srcdir)
