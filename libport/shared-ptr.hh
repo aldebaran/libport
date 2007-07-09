aa20f99ef1e5f90ae189e484a6668bb48e0536ff
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-07-09 08:44:41 +0000

    Ref couted pointer on top of boost 	* shared-ref.hh, shared-ref.hxx: New.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@1001 f9a80dcf-8710-0410-9714-9a4fb6817a1c

---
+++ libport/shared-ptr.hh
@@ -0,0 +1,95 @@
+/**
+ ** \file libport/shared_ptr.hh
+ ** \brief Declaration of libport::shared_ptr.
+ **/
+
+#ifndef LIBPORT_SHARED_REF_HH
+# define LIBPORT_SHARED_REF_HH
+
+# include <boost/shared_ptr.hpp>
+
+namespace libport
+{
+
+  /// A boost::shared_ptr wrapper.
+  ///
+  /// Compared to its super type, this implementation provides
+  /// cast operators, and implicit constructors.
+  template <typename T>
+  class shared_ptr : public boost::shared_ptr<T>
+  {
+  public:
+    /// The parent class.
+    typedef boost::shared_ptr<T> super_type;
+    /// The type pointed to.
+    typedef T element_type;
+
+    /// \name Constructors & Destructor.
+    /// \{
+    /** \brief Construct a new reference to the value pointed to by \a other.
+     ** The new reference shares the property of the object with \a other. */
+    template <typename U>
+    shared_ptr (const shared_ptr<U>& other);
+
+    /** \brief Copy constructor.
+     **
+     ** Although the implementation is subsumed by the previous, more
+     ** generic one, the C++ standard still mandates this specific
+     ** signature.  Otherwise, the compiler will provide a default
+     ** implementation, which is of course wrong.  Note that the
+     ** same applies for the assignment operator. */
+    shared_ptr (const shared_ptr<T>& other);
+
+    /** \brief Construct a counted reference to a newly allocated object.
+     ** The new reference takes the property of the object pointed to
+     ** by \a p.  If \a p is NULL, then the reference is invalid and
+     ** must be \c reset () before use. */
+    shared_ptr (T* p = 0);
+
+    /** \brief Destroy a reference.
+     ** The object pointed to is destroyed iff it is not referenced anymore. */
+    ~shared_ptr ();
+    /// \}
+
+    /// \name Equality operators.
+    /// \{
+
+    /** \brief Reference comparison.
+     ** Returns true if this points to \a p. */
+    bool operator== (const T* p) const;
+
+    /** \brief Reference comparison.
+     ** Returns false if this points to \a p. */
+    bool operator!= (const T* p) const;
+
+    /// \}
+
+    /// \name Casts.
+    /// \{
+
+    /** \brief Cast the reference.
+     ** Return a new reference, possibly throwing an exception if the
+     ** dynamic_cast is invalid.
+     **/
+    template <typename U> shared_ptr<U> cast () const;
+
+    /** \brief Cast the reference (unsafe).
+     ** Return a new reference, possibly a NULL reference if the
+     ** dynamic_cast is invalid.
+     **/
+    template <typename U> shared_ptr<U> unsafe_cast () const;
+    /// \}
+
+    /** \brief Test fellowship.
+     ** Return true if the reference points to an object which is
+     ** really of the specified type.
+     **/
+    template <typename U> bool is_a () const;
+  };
+
+
+}
+
+# include "libport/shared-ptr.hxx"
+
+#endif // !LIBPORT_SHARED_REF_HH
---
+++ libport/shared-ptr.hxx
@@ -0,0 +1,95 @@
+/**
+ ** \file libport/shared_ptr.hxx
+ ** \brief Implementation of libport::shared_ptr.
+ **/
+
+#ifndef LIBPORT_SHARED_REF_HXX
+# define LIBPORT_SHARED_REF_HXX
+
+# include "libport/shared-ptr.hh"
+
+namespace libport
+{
+
+  /*-----------------.
+  | Ctors and dtor.  |
+  `-----------------*/
+
+  template <typename T>
+  template <typename U>
+  shared_ptr<T>::shared_ptr (const shared_ptr<U>& other)
+    : super_type (other)
+  {
+  }
+
+  template <typename T>
+  shared_ptr<T>::shared_ptr (const shared_ptr<T>& other)
+    : super_type (other)
+  {
+  }
+
+  template <typename T>
+  shared_ptr<T>::shared_ptr (T* p)
+    : super_type (p)
+  {
+  }
+
+  template <typename T>
+  shared_ptr<T>::~shared_ptr ()
+  {
+  }
+
+
+  /*---------------------.
+  | Equality operators.  |
+  `---------------------*/
+
+  template <typename T>
+  bool
+  shared_ptr<T>::operator== (const T* other) const
+  {
+    return this->get () == other;
+  }
+
+  template <typename T>
+  bool
+  shared_ptr<T>::operator!= (const T* other) const
+  {
+    return !(*this == other);
+  }
+
+
+  /*--------.
+  | Casts.  |
+  `--------*/
+
+  template <typename T>
+  template <typename U>
+  shared_ptr<U>
+  shared_ptr<T>::unsafe_cast () const
+  {
+    shared_ptr<U> res;
+    (boost::dynamic_pointer_cast<U, element_type> (*this)).swap (res);
+    return res;
+  }
+
+  template <typename T>
+  template <typename U>
+  shared_ptr<U>
+  shared_ptr<T>::cast () const
+  {
+    if (!this->get () || !this->is_a<U> ())
+      throw std::bad_cast ();
+    return unsafe_cast<U> ();
+  }
+
+  template <typename T>
+  template <typename U>
+  bool
+  shared_ptr<T>::is_a () const
+  {
+    return dynamic_cast<U*> (this->get ());
+  }
+}
+
+#endif // !LIBPORT_SHARED_REF_HXX
