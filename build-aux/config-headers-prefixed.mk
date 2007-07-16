c8af0256104e5bbab0842e1316ff935fc82538c4
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-07-16 08:38:34 +0000

    Simplify libport handling 	* config-headers-prefixed.mk: This file must be included for 	the local Makefile.am only, no longer from the top level Makefile.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@1029 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/config-headers-prefixed.mk
+++ build-aux/config-headers-prefixed.mk
@@ -1,16 +1,14 @@
-# Use this file only from the top-level Makefile, otherwise
-# it won't produce the expect result.
-
-BUILT_SOURCES += $(PREFIXED_CONFIG_HEADER)
+# Use this file only from the Makefile having the prefixed config.h.
+BUILT_SOURCES += config.h
 
 # There is no point in maintaining a stamp file, Automake already does
 # this for config.h.  Arguably, we could use _configs.sed, but it is
 # not documented.
 
-$(PREFIXED_CONFIG_HEADER): $(CONFIG_HEADER)
+config.h: $(CONFIG_HEADER)
 	cd $(top_builddir) &&					\
 	$(SHELL) ./config.status $(PREFIXED_CONFIG_HEADER)
 
 CLEANFILES +=					\
 _configs.sed					\
-$(PREFIXED_CONFIG_HEADER)
+config.h
