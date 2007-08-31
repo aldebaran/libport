3e91d22f381527c519e85f32a151350d6aa69354
deslandes <deslandes@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-08-31 09:44:31 +0000

    Build doc in builddir not in srcdir 	* doxygen.mk: Here. 	add all-local rule to force doc generation.
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@1101 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- build-aux/doxygen.mk
+++ build-aux/doxygen.mk
@@ -6,30 +6,33 @@
 ## ----------------------- ##
 
 .PHONY: doc internal-doc user-doc
+
+all-local: doc
+
 doc: internal-doc
 
 internal-doc:
-	rm -rf $(DOCDIR).tmp $(srcdir)/$(DOCDIR)
-	$(MAKE) $(AM_MAKEFLAGS) DOC_INTERNAL=YES $(srcdir)/$(DOCDIR)
+	rm -rf $(DOCDIR).tmp $(builddir)/$(DOCDIR)
+	$(MAKE) $(AM_MAKEFLAGS) DOC_INTERNAL=YES $(builddir)/$(DOCDIR)
 
 user-doc:
-	rm -rf $(DOCDIR).tmp $(srcdir)/$(DOCDIR)
-	$(MAKE) $(AM_MAKEFLAGS) DOC_INTERNAL=NO $(srcdir)/$(DOCDIR)
+	rm -rf $(DOCDIR).tmp $(builddir)/$(DOCDIR)
+	$(MAKE) $(AM_MAKEFLAGS) DOC_INTERNAL=NO $(builddir)/$(DOCDIR)
 
-EXTRA_DIST = $(srcdir)/$(DOCDIR)
-$(srcdir)/$(DOCDIR): $(top_srcdir)/build-aux/Doxyfile.in
-	test -w $(srcdir)
-	rm -rf $(DOCDIR).tmp $(srcdir)/$(DOCDIR)
+EXTRA_DIST = $(builddir)/$(DOCDIR)
+$(builddir)/$(DOCDIR): $(top_srcdir)/build-aux/Doxyfile.in
+	test -w $(builddir)
+	rm -rf $(DOCDIR).tmp $(builddir)/$(DOCDIR)
 	$(MAKE) $(AM_MAKEFLAGS) Doxyfile
 	sed -e 's/@DOC_INTERNAL@/$(DOC_INTERNAL)/g' \
             -e 's,@EXCLUDE_PATTERNS@,$(EXCLUDE_PATTERNS),g' Doxyfile >Doxytmp
 	mv -f Doxytmp Doxyfile
 	chmod -w Doxyfile
 	$(DOXYGEN) Doxyfile
-	mv $(DOCDIR).tmp $(srcdir)/$(DOCDIR)
+	mv $(DOCDIR).tmp $(builddir)/$(DOCDIR)
 
 maintainer-clean-local:
-	rm -rf $(DOCDIR).tmp $(srcdir)/$(DOCDIR)
+	rm -rf $(DOCDIR).tmp $(builddir)/$(DOCDIR)
 
 # We install by hand, otherwise Automake produces "install .../srcdoc
 # .../srcdoc", which install our dir into the previous one, instead of
@@ -37,7 +40,7 @@ maintainer-clean-local:
 install-data-local:
 	rm -rf $(DESTDIR)$(htmldir)/$(DOCDIR)
 	$(mkdir_p) $(DESTDIR)$(htmldir)
-	cp -r $(srcdir)/$(DOCDIR) $(DESTDIR)$(htmldir)
+	cp -r $(builddir)/$(DOCDIR) $(DESTDIR)$(htmldir)
 
 uninstall-local:
 	chmod -R 700 $(DESTDIR)$(htmldir)/$(DOCDIR)
