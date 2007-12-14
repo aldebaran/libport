453634e666d50a928882b237acc31dcec7cb723b
demaille <demaille@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-12-14 11:12:45 +0000

    Prefer enums to #define.
    	* sysexits.hh: here.
    
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@1528 f9a80dcf-8710-0410-9714-9a4fb6817a1c

--- libport/sysexits.hh
+++ libport/sysexits.hh
@@ -4,27 +4,30 @@
 #ifndef LIBPORT_SYSEXIT_HH
 # define LIBPORT_SYSEXIT_HH
 
-# ifdef LIBPORT_HAVE_SYSEXIT_H
-#  include <sysexits.h>
-# else
-#  define EX_OK		   0	/* successful termination */
-#  define EX__BASE	   64	/* base value for error messages */
-#  define EX_USAGE	   64	/* command line usage error */
-#  define EX_DATAERR	   65	/* data format error */
-#  define EX_NOINPUT	   66	/* cannot open input */
-#  define EX_NOUSER	   67	/* addressee unknown */
-#  define EX_NOHOST	   68	/* host name unknown */
-#  define EX_UNAVAILABLE   69	/* service unavailable */
-#  define EX_SOFTWARE	   70	/* internal software error */
-#  define EX_OSERR	   71	/* system error (e.g., can't fork) */
-#  define EX_OSFILE	   72	/* critical OS file missing */
-#  define EX_CANTCREAT	   73	/* can't create (user) output file */
-#  define EX_IOERR	   74	/* input/output error */
-#  define EX_TEMPFAIL	   75	/* temp failure; user is invited to retry */
-#  define EX_PROTOCOL	   76	/* remote error in protocol */
-#  define EX_NOPERM	   77	/* permission denied */
-#  define EX_CONFIG	   78	/* configuration error */
-#  define EX__MAX	   78	/* maximum listed value */
-# endif
+//# ifdef LIBPORT_HAVE_SYSEXIT_H
+//#  include <sysexits.h>
+//# else
+enum sysexit
+{
+  EX_OK		   =  0, //< successful termination
+  EX__BASE	   = 64, //< base value for error messages
+  EX_USAGE	   = 64, //< command line usage error
+  EX_DATAERR	   = 65, //< data format error
+  EX_NOINPUT	   = 66, //< cannot open input
+  EX_NOUSER	   = 67, //< addressee unknown
+  EX_NOHOST	   = 68, //< host name unknown
+  EX_UNAVAILABLE   = 69, //< service unavailable
+  EX_SOFTWARE	   = 70, //< internal software error
+  EX_OSERR	   = 71, //< system error (e.g., can't fork)
+  EX_OSFILE	   = 72, //< critical OS file missing
+  EX_CANTCREAT	   = 73, //< can't create (user) output file
+  EX_IOERR	   = 74, //< input/output error
+  EX_TEMPFAIL	   = 75, //< temp failure; user is invited to retry
+  EX_PROTOCOL	   = 76, //< remote error in protocol
+  EX_NOPERM	   = 77, //< permission denied
+  EX_CONFIG	   = 78, //< configuration error
+  EX__MAX	   = 78, //< maximum listed value
+};
+//# endif
 
 #endif // ! LIBPORT_SYSEXIT_HH
