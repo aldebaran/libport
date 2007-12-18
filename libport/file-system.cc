791b43d3d9947ab464a1c2350824b4e1e2407f3f
fosse <fosse@f9a80dcf-8710-0410-9714-9a4fb6817a1c>
2007-12-18 14:02:48 +0000

    Add file-system containing is_dir.
    	* Makefile.am, libport-sources.mk: Adjust.
    	* file-system.cc, file-system.hh: New.
    
    git-svn-id: https://svn.gostai.com/svn/common/project-template/trunk@1563 f9a80dcf-8710-0410-9714-9a4fb6817a1c

---
+++ libport/file-system.cc
@@ -0,0 +1,17 @@
+/** 
+ ** \file libport/file-system.cc
+ ** \brief Implementation for file-system.hh
+ */
+
+#include "libport/sys/stat.h"
+#include "libport/file-system.hh"
+
+namespace libport
+{
+  bool is_dir (const std::string& f)
+  {
+    struct stat st;
+    stat(f.c_str(), &st);
+    return S_ISDIR(st.st_mode);
+  }
+}
---
+++ libport/file-system.hh
@@ -0,0 +1,16 @@
+/**
+ ** \file libport/file-system.hh
+ ** \brief Provides misc functions to handle filesystem
+ */
+
+#ifndef LIBPORT_FILE_SYSTEM_HH
+# define LIBPORT_FILE_SYSTEM_HH
+
+#include <string>
+
+namespace libport
+{
+  bool is_dir (const std::string& f);
+}
+
+#endif
