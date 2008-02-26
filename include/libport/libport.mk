8f11859e294d32f90b79272ca5877cabe02f5bbc
Fabrice Hesling <hesling@gostai.com>
2008-02-26 14:09:05 +0100

    Export _errlist.hh
        * include/libport/libport.mk: Here.

--- include/libport/libport.mk
+++ include/libport/libport.mk
@@ -15,6 +15,7 @@ libport_include_HEADERS =			\
 
 # C++ headers.
 libport_include_HEADERS +=			\
+	$(include_libport)/_errlist.hh		\
 	$(include_libport)/assert.hh		\
 	$(include_libport)/cli.hh		\
 	$(include_libport)/cli.hxx		\
