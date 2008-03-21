b68ee0942253c3edbae6cf615ac70468ecdf0dfc
Nicolas Despres <despres@gostai.com>
2008-03-21 14:27:11 +0100

    Extend and adjust project information.
    	* build-aux/cmake/modules/CheckCMakeVarsExists.cmake: New.
    	* build-aux/cmake/modules/ProjectInfo.cmake: New.
    	* build-aux/cmake/modules/project-info.h.in: Declare PACKAGE_* instead
    	of PROJECT_*. Add more variables need by libport::PackageInfo.
    
    Signed-off-by: Nicolas Despres <despres@gostai.com>

---
+++ build-aux/cmake/modules/CheckCMakeVarsExists.cmake
@@ -0,0 +1,25 @@
+##
+## CheckCMakeVarsExists.cmake: This file is part of libport.
+## Copyright (C) Gostai S.A.S., 2006-2008.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+## For comments, bug reports and feedback: http://www.urbiforge.com
+##
+
+if(NOT COMMAND CHECK_CMAKE_VARS_EXISTS)
+
+  # Check whether the given variables exists. Send an error message if it
+  # doesn't.
+  macro(CHECK_CMAKE_VARS_EXISTS)
+    foreach(i ${ARGV})
+      if(NOT DEFINED ${i})
+	message(SEND_ERROR "Variables '${i}' must be defined.")
+      endif()
+    endforeach(i)
+  endmacro(CHECK_CMAKE_VARS_EXISTS)
+
+endif(NOT COMMAND CHECK_CMAKE_VARS_EXISTS_CMAKE)
---
+++ build-aux/cmake/modules/ProjectInfo.cmake
@@ -0,0 +1,26 @@
+##
+## ProjectInfo.cmake: This file is part of libport.
+## Copyright (C) Gostai S.A.S., 2006-2008.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+## For comments, bug reports and feedback: http://www.urbiforge.com
+##
+
+include(CheckCMakeVarsExists)
+
+if(NOT PROJECT_INFO_CMAKE)
+  set(PROJECT_INFO_CMAKE TRUE)
+
+  check_cmake_vars_exists(PROJECT_VERSION_MAJOR PROJECT_VERSION_MINOR)
+  set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
+
+  check_cmake_vars_exists(PROJECT_LONGNAME PROJECT_VERSION)
+  set(PROJECT_STRING "${PROJECT_LONGNAME} ${PROJECT_VERSION}")
+
+  set(PROJECT_TARNAME "${PROJECT_NAME}-${PROJECT_VERSION}")
+
+endif(NOT PROJECT_INFO_CMAKE)
