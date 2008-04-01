ffd37f6ef8092ebea6d0378c068da5b9dd79feba
Nicolas Despres <despres@gostai.com>
2008-04-01 14:35:17 +0200

    Rename URBI constant to URBI_NAME.
    	The URBI constant is used in sdk-remote.
    
    	* build-aux/cmake/modules/GostaiInfo.cmake: Do that.
    	* build-aux/cmake/modules/gostai-info.texi.in: Adjust.
    
    Signed-off-by: Nicolas Despres <despres@gostai.com>

--- build-aux/cmake/modules/GostaiInfo.cmake
+++ build-aux/cmake/modules/GostaiInfo.cmake
@@ -16,7 +16,7 @@ if(NOT DEFINED GOSTAI_INFO_CMAKE)
 set(GOSTAI_INFO_CMAKE TRUE)
 
 set(GOSTAI "Gostai")
-set(URBI "Urbi")
+set(URBI_NAME "Urbi")
 set(GOSTAI_HOMEPAGE "http://www.gostai.com")
 set(GOSTAI_COMMUNITY_URL "http://www.urbiforge.com")
 set(GOSTAI_FORUM_URL "http://forum.gostai.com")
