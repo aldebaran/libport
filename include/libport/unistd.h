18111f096762fddf9b00d7d75fb1f8b3addf8906
Akim Demaille <demaille@gostai.com>
2008-05-27 14:58:14 +0200

    Fix header inclusion.
    	* include/libport/unistd.h: Use <>, not "".
    	Qualify paths.

--- include/libport/unistd.h
+++ include/libport/unistd.h
@@ -1,9 +1,9 @@
 #ifndef LIBPORT_UNISTD_H
 # define LIBPORT_UNISTD_H
 
-# include "detect_win32.h"
+# include <libport/detect_win32.h>
 
-# include "libport/config.h"
+# include <libport/config.h>
 
 // This is traditional Unix file.
 # ifdef LIBPORT_HAVE_UNISTD_H
@@ -13,7 +13,7 @@
 // This seems to be its WIN32 equivalent.
 // http://msdn2.microsoft.com/en-us/library/ms811896(d=printer).aspx#ucmgch09_topic7.
 # if defined WIN32 || defined LIBPORT_WIN32
-#  include "io.h"
+#  include <io.h>
 # endif
 
 #if defined LIBPORT_HAVE__GETCWD
