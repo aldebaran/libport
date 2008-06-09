5e340e6a091ea86d56e89782de7680b522dc5f33
Samuel Tardieu <sam@rfc1149.net>
2008-06-09 23:33:04 +0200

    Add test for libport::any.
    	* tests/libport/any.cc: New.
    	* tests/libport/Makefile.am: Update.

---
+++ tests/libport/any.cc
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <vector>
+#include <boost/lambda/lambda.hpp>
+#include <libport/containers.hh>
+
+static std::vector<int> v;
+
+static void
+check(int i, bool b)
+{
+  assert(b == libport::any(v, boost::lambda::_1 == i));
+}
+
+int main()
+{
+  v.push_back(1);
+  v.push_back(2);
+  check(1, true);
+  check(2, true);
+  check(3, false);
+  return 0;
+}
