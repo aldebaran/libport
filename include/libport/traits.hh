ebc77ac5e5e5979c71e9acb4bcb425ab6ad2beb8
mefyl <hocquet@gostai.com>
2008-07-16 18:28:28 +0200

    Add shared pointer traits.
        * include/libport/traits.hh: Here.

--- include/libport/traits.hh
+++ include/libport/traits.hh
@@ -1,6 +1,8 @@
 #ifndef LIBPORT_TRAITS_HH
 # define LIBPORT_TRAITS_HH
 
+# include <libport/shared-ptr.hh>
+
 namespace libport
 {
   namespace traits
@@ -8,37 +10,49 @@ namespace libport
     template <typename T>
     struct Id
     {
-        typedef T res;
+      typedef T res;
     };
 
     template <typename T>
     struct Const
     {
-        typedef const T res;
+      typedef const T res;
     };
 
     template <typename T>
     struct Ptr
     {
-        typedef T* res;
+      typedef T* res;
     };
 
     template <typename T>
     struct Ref
     {
-        typedef T& res;
+      typedef T& res;
     };
 
     template <typename T>
     struct ConstPtr
     {
-        typedef typename meta::Compose<Ptr, Const>::Res<T>::res res;
+      typedef typename meta::Compose<Ptr, Const>::Res<T>::res res;
     };
 
     template <typename T>
     struct ConstRef
     {
-        typedef typename meta::Compose<Ref, Const>::Res<T>::res res;
+      typedef typename meta::Compose<Ref, Const>::Res<T>::res res;
+    };
+
+    template <typename T>
+    struct SharedPtr
+    {
+      typedef typename libport::shared_ptr<T> res;
+    };
+
+    template <typename T>
+    struct ConstSharedPtr
+    {
+      typedef typename meta::Compose<SharedPtr, Const>::Res<T>::res res;
     };
   }
 }
