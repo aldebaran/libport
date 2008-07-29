fb6acf2c451942f450688d82485757f061dcb16e
Akim Demaille <demaille@gostai.com>
2008-07-29 10:57:32 +0200

    convert_envvar.
    	* include/libport/cli.hh, include/libport/cli.hxx: Formatting
    	changes.
    	(convert_envvar): New.

--- include/libport/cli.hh
+++ include/libport/cli.hh
@@ -11,23 +11,29 @@
 namespace libport
 {
   ATTRIBUTE_NORETURN
-  void usage_error (const std::string& opt, const std::string& err);
+  void usage_error(const std::string& opt, const std::string& err);
 
   ATTRIBUTE_NORETURN
-  void required_argument (const std::string& opt);
+  void required_argument(const std::string& opt);
 
   ATTRIBUTE_NORETURN
-  void missing_argument (const std::string& opt);
+  void missing_argument(const std::string& opt);
 
   ATTRIBUTE_NORETURN
-  void invalid_option (const std::string& opt);
+  void invalid_option(const std::string& opt);
 
   ATTRIBUTE_NORETURN
-  void invalid_option_val (const std::string& opt, const std::string& arg);
+  void invalid_option_val(const std::string& opt, const std::string& arg);
 
-  // Can be used on a const char* to simply test for argument presence.
+  /// Can be used on with T = const char* to simply require that \a arg
+  /// is nonnull.
   template<typename T>
-  T convert_argument (const std::string& opt, const std::string& arg);
+  T convert_argument(const std::string& opt, const std::string& arg);
+
+  /// Convert the value contained in \a envvar to a \a T.
+  /// \precondition getenv(envvar).
+  template<typename T>
+  T convert_envvar(const std::string& envvar);
 }
 
 # include <libport/cli.hxx>
--- include/libport/cli.hxx
+++ include/libport/cli.hxx
@@ -7,10 +7,10 @@ namespace libport
 
   template<typename T>
   T
-  convert_argument (const std::string& opt, const char* arg)
+  convert_argument(const std::string& opt, const char* arg)
   {
     if (!arg)
-      required_argument (opt);
+      required_argument(opt);
 
     try
     {
@@ -18,7 +18,7 @@ namespace libport
     }
     catch (boost::bad_lexical_cast&)
     {
-      invalid_option_val (opt, arg);
+      invalid_option_val(opt, arg);
     }
   }
 
@@ -27,7 +27,15 @@ namespace libport
   convert_argument<>(const std::string& opt, const char* arg)
   {
     if (!arg)
-      required_argument (opt);
+      required_argument(opt);
     return arg;
+  }
+
+  template<typename T>
+  T
+  convert_envvar(const std::string& envvar)
+  {
+    return convert_argument<T>(std::string("$") + envvar,
+                               getenv(envvar.c_str()));
   }
 }
