9e493ab60fc8b8bc0acfa4f1596a321bfd86290b
Clement PETIT <petit@gostai.com>
2008-07-30 11:05:23 +0200

    Change of typo.
        Remove trailing whitespaces. No space at
        begin and end of brackets.
    
        * build-aux/cmake/modules/Macro-toolbox.cmake: Here.
        * build-aux/cmake/modules/UProject.cmake: Here.

--- build-aux/cmake/modules/Macro-toolbox.cmake
+++ build-aux/cmake/modules/Macro-toolbox.cmake
@@ -1,8 +1,3 @@
-## --------------------------------------------------------------
-## CMake - Macro toolbox    
-## The aim of this file is to group usefull for
-## user-defined macro
-## --------------------------------------------------------------
 ## Copyright (C) Gostai S.A.S., 2006-2008.
 ##
 ## This software is provided "as is" without warranty of any kind,
@@ -11,6 +6,11 @@
 ##
 ## See the LICENSE file for more information.
 ## For comments, bug reports and feedback: http://www.urbiforge.com
+## --------------------------------------------------------------
+## CMake - Macro toolbox    
+## The aim of this file is to group usefull for
+## user-defined macro
+## --------------------------------------------------------------
 ##
 
 #-----------------------------------------------------------------------------------------
@@ -20,13 +20,13 @@
 # Example :
 # ---------
 
-# SET ( arguments
+# SET(arguments
 #   hello OPTION3 world
 #   LIST3 foo bar
 #   OPTION2
 #   LIST1 fuz baz
 #   )
-# parse_arguments ( ARG "LIST1;LIST2;LIST3" "OPTION1;OPTION2;OPTION3" ${arguments} )
+# parse_arguments(ARG "LIST1;LIST2;LIST3" "OPTION1;OPTION2;OPTION3" ${arguments})
 
 # * results
 #     > ARG_DEFAULT_ARGS: hello;world
@@ -38,47 +38,47 @@
 #     > ARG_OPTION3: TRUE 
 
 
-MACRO ( parse_arguments prefix arg_names option_names )
+MACRO (parse_arguments prefix arg_names option_names)
 
-  set ( DEFAULT_ARGS )
-  foreach ( arg_name ${arg_names} )    
-    set ( ${prefix}_${arg_name} )
-  endforeach ( arg_name )
+  set (DEFAULT_ARGS)
+  foreach (arg_name ${arg_names})
+    set (${prefix}_${arg_name})
+  endforeach (arg_name)
   
-  foreach ( option ${option_names} )
-    set ( ${prefix}_${option} FALSE )
-  endforeach ( option )
+  foreach (option ${option_names})
+    set (${prefix}_${option} FALSE)
+  endforeach (option)
 
-  set ( current_arg_name DEFAULT_ARGS )
-  set ( current_arg_list )
+  set (current_arg_name DEFAULT_ARGS)
+  set (current_arg_list)
   
-  foreach  ( arg ${ARGN} )            
-    set ( larg_names ${arg_names} )    
-    list ( FIND larg_names "${arg}" is_arg_name )                   
-  
-    if ( is_arg_name GREATER -1 )
-  
-      set ( ${prefix}_${current_arg_name} ${current_arg_list} )
-      set ( current_arg_name ${arg} )
-      set ( current_arg_list )
-  
-    else ( is_arg_name GREATER -1)
-  
-      set ( loption_names ${option_names} )    
-      list ( FIND loption_names "${arg}" is_option )
-  
-      if ( is_option GREATER -1 )
-	
-	     set ( ${prefix}_${arg} TRUE )
-      else ( is_option GREATER -1 )
-	     set ( current_arg_list ${current_arg_list} ${arg} )
-  
-      endif ( is_option GREATER -1 )
-  
-    endif ( is_arg_name GREATER -1 )
-  
-  endforeach ( arg )
-  
-  set ( ${prefix}_${current_arg_name} ${current_arg_list} )
+  foreach (arg ${ARGN})
+    set (larg_names ${arg_names})
+    list (FIND larg_names "${arg}" is_arg_name)
 
-ENDMACRO( parse_arguments )
+    if (is_arg_name GREATER -1)
+
+      set (${prefix}_${current_arg_name} ${current_arg_list})
+      set (current_arg_name ${arg})
+      set (current_arg_list)
+
+    else (is_arg_name GREATER -1)
+  
+      set (loption_names ${option_names})
+      list (FIND loption_names "${arg}" is_option)
+
+      if (is_option GREATER -1)
+
+	     set (${prefix}_${arg} TRUE)
+      else (is_option GREATER -1)
+	     set (current_arg_list ${current_arg_list} ${arg})
+
+      endif (is_option GREATER -1)
+
+    endif (is_arg_name GREATER -1)
+
+  endforeach (arg)
+  
+  set (${prefix}_${current_arg_name} ${current_arg_list})
+
+ENDMACRO (parse_arguments)
--- build-aux/cmake/modules/UProject.cmake
+++ build-aux/cmake/modules/UProject.cmake
@@ -1,7 +1,3 @@
-## --------------------------------------------------------------- 
-## CMake - UObject project
-## User-friendly macros for UObject project.
-## ------------------------------- -------------------------------
 ## Copyright (C) Gostai S.A.S., 2006-2008.
 ##
 ## This software is provided "as is" without warranty of any kind,
@@ -10,41 +6,45 @@
 ##
 ## See the LICENSE file for more information.
 ## For comments, bug reports and feedback: http://www.urbiforge.com
+## --------------------------------------------------------------- 
+## CMake - UObject project
+## User-friendly macros for UObject project.
+## ------------------------------- -------------------------------
 ##
 
-include ( Macro-toolbox )
+include (Macro-toolbox)
 # parse_arguments macro
 
 #-----------------------------------------------------------------------------------------
 #                                     Add Remote UObject
 #-----------------------------------------------------------------------------------------
 # Create a remote UObject
-# e.g. add_remote ( name_of_binary SOURCES cpp1 cpp2.. [DEPENDS target1 target2..] [NO_DEFAULT_MAIN] )
+# e.g. add_remote (name_of_binary SOURCES cpp1 cpp2.. [DEPENDS target1 target2..] [NO_DEFAULT_MAIN])
 
-MACRO ( add_remote )
+MACRO (add_remote)
 
-    parse_arguments ( REMOTE_UOBJECT "SOURCES;DEPENDS" "NO_DEFAULT_MAIN" ${ARGN} )
-    set ( REMOTE_UOBJECT_NAME ${REMOTE_UOBJECT_DEFAULT_ARGS} )
-    add_executable ( ${REMOTE_UOBJECT_NAME} ${REMOTE_UOBJECT_SOURCES} )
-    link_uobject_libraries ( ${REMOTE_UOBJECT_NAME} )    
-    target_link_libraries ( ${REMOTE_UOBJECT_NAME} ${REMOTE_UOBJECT_DEPENDS} )
+    parse_arguments (REMOTE_UOBJECT "SOURCES;DEPENDS" "NO_DEFAULT_MAIN" ${ARGN})
+    set (REMOTE_UOBJECT_NAME ${REMOTE_UOBJECT_DEFAULT_ARGS})
+    add_executable (${REMOTE_UOBJECT_NAME} ${REMOTE_UOBJECT_SOURCES})
+    link_uobject_libraries (${REMOTE_UOBJECT_NAME})    
+    target_link_libraries (${REMOTE_UOBJECT_NAME} ${REMOTE_UOBJECT_DEPENDS})
 
-ENDMACRO ( add_remote )
+ENDMACRO (add_remote)
 
 #-----------------------------------------------------------------------------------------
 #                                      Add Engine library
 #-----------------------------------------------------------------------------------------
 # Creates a library ready to be linked to an engine kernel.
-# e.g. add_engine_library ( name_of_binary SOURCES cpp1 cpp2.. [DEPENDS target1 target2..] [NO_DEFAULT_MAIN] )
+# e.g. add_engine_library (name_of_binary SOURCES cpp1 cpp2.. [DEPENDS target1 target2..] [NO_DEFAULT_MAIN])
 
-MACRO ( add_engine_library )
+MACRO (add_engine_library)
 
-    parse_arguments ( ENGINE_UOBJECT "SOURCES;DEPENDS" "NO_DEFAULT_MAIN" ${ARGN} )
-    set ( ENGINE_UOBJECT_NAME ${ENGINE_UOBJECT_DEFAULT_ARGS} )
-    add_library ( ${ENGINE_UOBJECT_NAME} ${ENGINE_UOBJECT_SOURCES} )
-    target_link_libraries ( ${ENGINE_UOBJECT_NAME} ${ENGINE_UOBJECT_DEPENDS} )    
+    parse_arguments (ENGINE_UOBJECT "SOURCES;DEPENDS" "NO_DEFAULT_MAIN" ${ARGN})
+    set (ENGINE_UOBJECT_NAME ${ENGINE_UOBJECT_DEFAULT_ARGS})
+    add_library (${ENGINE_UOBJECT_NAME} ${ENGINE_UOBJECT_SOURCES})
+    target_link_libraries (${ENGINE_UOBJECT_NAME} ${ENGINE_UOBJECT_DEPENDS})
 
-ENDMACRO ( add_engine_library )
+ENDMACRO (add_engine_library)
 
 #-----------------------------------------------------------------------------------------
 #                                      Add UObject
@@ -53,23 +53,23 @@ ENDMACRO ( add_engine_library )
 # It MUST be called in a project loaded by 'add_project' command to work properly.
 # You can specify options into add_uobject brackets that will be applied to both uobject.
 
-MACRO ( add_uobject )
+MACRO (add_uobject)
 
-    add_remote( 
+    add_remote(
         ${PROJECT_NAME}${UOBJECT_REMOTE_SUFFIX} 
-        SOURCES ${${PROJECT_NAME}_UOBJECT_SOURCES} 
+        SOURCES ${${PROJECT_NAME}_UOBJECT_SOURCES}
         DEPENDS ${PROJECT_LINKS}
         ${ARGN}
-    )
+   )
 
     add_engine_library(
         ${PROJECT_NAME}${UOBJECT_ENGINE_LIBRARY_SUFFIX}
         SOURCES ${${PROJECT_NAME}_UOBJECT_SOURCES} ${${PROJECT_NAME}_WRAPPER_SOURCES}
         DEPENDS ${PROJECT_LINKS}
         ${ARGN}
-    )
+   )
 
-ENDMACRO ( add_uobject )
+ENDMACRO (add_uobject)
 
 
 #-----------------------------------------------------------------------------------------
@@ -85,79 +85,84 @@ ENDMACRO ( add_uobject )
 # Call this function in the root CMakeLists.txt to add a project. If it's a standalone
 # project, it will be equivalent to add_subdirectory command.
 # However, if the project is linked to others just notice and all will be managed.
-# e.g. add_project ( project_folder [DEPENDS project_folder1 project_folder2..] )
+# e.g. add_project (project_folder [DEPENDS project_folder1 project_folder2..])
 
-MACRO ( add_project )
+MACRO (add_project)
 
-    parse_arguments ( PROJECT "DEPENDS" "" ${ARGN} )
-    set ( PROJECT_DIR ${PROJECT_DEFAULT_ARGS} )
-    set ( PROJECT_LINKS )
-    
-    if ( PROJECT_DEPENDS )
-        foreach ( PROJECT_DEPENDS_DIR ${PROJECT_DEPENDS} ) 
-            include_directories ( ${MODULES_SOURCE_DIR}/${PROJECT_DEPENDS_DIR} )
-            link_directories ( ${MODULES_SOURCE_DIR}/${PROJECT_DEPENDS_DIR} )
-            set ( PROJECT_LINKS ${PROJECT_LINKS} ${PROJECT_DEPENDS}${UOBJECT_ENGINE_LIBRARY_SUFFIX} )
-        endforeach ( PROJECT_DEPENDS_DIR )
-    endif ( PROJECT_DEPENDS )
-    
-    add_subdirectory ( ${PROJECT_DIR} )
-    
-ENDMACRO ( add_project )
+    parse_arguments (PROJECT "DEPENDS" "" ${ARGN})
+    set (PROJECT_DIR ${PROJECT_DEFAULT_ARGS})
+    set (PROJECT_LINKS)
+
+    if (PROJECT_DEPENDS)
+        foreach (PROJECT_DEPENDS_DIR ${PROJECT_DEPENDS}) 
+            include_directories (${MODULES_SOURCE_DIR}/${PROJECT_DEPENDS_DIR})
+            link_directories (${MODULES_SOURCE_DIR}/${PROJECT_DEPENDS_DIR})
+            set (PROJECT_LINKS ${PROJECT_LINKS} ${PROJECT_DEPENDS}${UOBJECT_ENGINE_LIBRARY_SUFFIX})
+        endforeach (PROJECT_DEPENDS_DIR)
+    endif (PROJECT_DEPENDS)
+
+    add_subdirectory (${PROJECT_DIR})
+
+ENDMACRO (add_project)
 
 #-----------------------------------------------------------------------------------------
 #                                   Link UObject libraries
 #-----------------------------------------------------------------------------------------
 # Link your current executable with all libraries required for a standard remote UObject
-# e.g. link_uobject_libraries ( uobject_name )
+# e.g. link_uobject_libraries (uobject_name)
 
-MACRO ( link_uobject_libraries UOBJECT_NAME )
-# e.g. "link_uobject_libraries ( NAME_OF_EXECUTABLE CPP1 CPP2 )"
+MACRO (link_uobject_libraries UOBJECT_NAME)
+# e.g. "link_uobject_libraries (NAME_OF_EXECUTABLE CPP1 CPP2)"
 
     # Autofind libraries
-    if ( WIN32 )
-      set ( CMAKE_FIND_LIBRARY_PREFIXES ${CMAKE_FIND_LIBRARY_PREFIXES} "" "lib" )
-    endif ( WIN32 )
+    if (WIN32)
+      set (CMAKE_FIND_LIBRARY_PREFIXES ${CMAKE_FIND_LIBRARY_PREFIXES} "" "lib")
+    endif (WIN32)
 
-    find_library ( SDK_REMOTE_LIBRARY urbi )
-    find_library ( JPEG_LIBRARY jpeg )
-    find_path ( SDK_REMOTE_INCLUDE_DIR urbi )
+    find_library (SDK_REMOTE_LIBRARY urbi)
+    find_library (JPEG_LIBRARY jpeg)
+    find_path (SDK_REMOTE_INCLUDE_DIR urbi)
 
     # Keeps only required libs
-    if ( WIN32 )
-      set ( CMAKE_CXX_STANDARD_LIBRARIES "gdi32.lib" )
-      set ( CMAKE_C_STANDARD_LIBRARIES "gdi32.lib")
-    endif ( WIN32 )
+    if (WIN32)
+      set (CMAKE_CXX_STANDARD_LIBRARIES "gdi32.lib")
+      set (CMAKE_C_STANDARD_LIBRARIES "gdi32.lib")
+    endif (WIN32)
 
     # FIXME // Do not force release mode actually
     # Force release mode only
-    if ( WIN32 )
-      set ( CMAKE_BUILD_TYPE "Release" )
-    endif ( WIN32 )
+    if (WIN32)
+      set (CMAKE_BUILD_TYPE "Release")
+    endif (WIN32)
 
     # Default libraries, in case they're not found
-    if ( NOT SDK_REMOTE_INCLUDE_DIR )
-          message ( "SDK Remote INCLUDE directory has not been found. Make sure you've installed URBI SDK remote on your computer or please specify the library path manually (-DSDK_REMOTE_INCLUDE_DIR)." )
-    endif ( NOT SDK_REMOTE_INCLUDE_DIR )
+    if (NOT SDK_REMOTE_INCLUDE_DIR)
+          message ("SDK Remote INCLUDE directory has not been found. Make sure you've installed "
+                   "URBI SDK remote on your computer or please specify the library path manually "
+                   "(-DSDK_REMOTE_INCLUDE_DIR).")
+    endif (NOT SDK_REMOTE_INCLUDE_DIR)
 
-    if ( NOT JPEG_LIBRARY )
-          message ( "The JPEG library has not been found. Make sure you've installed URBI SDK remote on your computer or please specify the library path manually (-DJPEG_LIBRARY)." )
-    endif ( NOT JPEG_LIBRARY )
+    if (NOT JPEG_LIBRARY)
+          message ("The JPEG library has not been found. Make sure you've installed URBI SDK remote "
+                   "on your computer or please specify the library path manually "
+                   "(-DJPEG_LIBRARY).")
+    endif (NOT JPEG_LIBRARY)
 
-    if ( NOT SDK_REMOTE_LIBRARY )
-          message ( "The URBI library has not been found. Make sure you've installed URBI SDK remote on your computer or please specify the library path manually (-DSDK_REMOTE_LIBRARY)." )
-    endif ( NOT SDK_REMOTE_LIBRARY )
+    if (NOT SDK_REMOTE_LIBRARY)
+          message ("The URBI library has not been found. Make sure you've installed URBI SDK remote "
+                   "on your computer or please specify the library path manually "
+                   "(-DSDK_REMOTE_LIBRARY).")
+    endif (NOT SDK_REMOTE_LIBRARY)
 
-    # Link 
-    include_directories ( ${SDK_REMOTE_INCLUDE_DIR} )
-    target_link_libraries ( ${UOBJECT_NAME} ${SDK_REMOTE_LIBRARY} ${JPEG_LIBRARY} )
-    if ( WIN32 )
-      target_link_libraries ( ${UOBJECT_NAME} ws2_32 )
-    elseif ( UNIX )
-      target_link_libraries ( ${UOBJECT_NAME} pthread )
-      else ( UNIX )
-        message ( "[UOBJECT] No auto-link has been pre-configured for your platform into CMake." )
-    endif ( WIN32 )
-        
-ENDMACRO ( link_uobject_libraries )
+    # Link
+    include_directories (${SDK_REMOTE_INCLUDE_DIR})
+    target_link_libraries (${UOBJECT_NAME} ${SDK_REMOTE_LIBRARY} ${JPEG_LIBRARY})
+    if (WIN32)
+      target_link_libraries (${UOBJECT_NAME} ws2_32)
+    elseif (UNIX)
+      target_link_libraries (${UOBJECT_NAME} pthread)
+      else (UNIX)
+        message ("[UOBJECT] No auto-link has been pre-configured for your platform into CMake.")
+    endif (WIN32)
 
+ENDMACRO (link_uobject_libraries)
