c70f5c01d001b8d606bb3d66d39ac66d44c1cf11
Nicolas Despres <despres@gostai.com>
2008-09-03 09:04:21 +0200

    Add Urbi studio and script name.
        * build-aux/cmake/modules/GostaiInfo.cmake,
        * build-aux/cmake/modules/gostai-info.h.in,
        * build-aux/cmake/modules/gostai-info.texi.in: Here.
    
    Signed-off-by: Nicolas Despres <despres@gostai.com>

--- build-aux/cmake/modules/GostaiInfo.cmake
+++ build-aux/cmake/modules/GostaiInfo.cmake
@@ -17,6 +17,8 @@ set(GOSTAI_INFO_CMAKE TRUE)
 
 set(GOSTAI "Gostai")
 set(URBI_NAME "Urbi")
+set(URBI_STUDIO_NAME "Urbi Studio")
+set(URBI_SCRIPT_NAME "urbiScript")
 set(GOSTAI_HOMEPAGE "http://www.gostai.com")
 set(GOSTAI_COMMUNITY_URL "http://www.urbiforge.com")
 set(GOSTAI_FORUM_URL "http://forum.gostai.com")
