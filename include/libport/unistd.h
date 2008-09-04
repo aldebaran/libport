4ebed3ecad1a7d110c7d2f0fadeebb09a244c8e1
Akim Demaille <demaille@gostai.com>
2008-09-04 11:39:13 +0200

    O_LARGEFILE portability.
    	* include/libport/unistd.h: Define it if needed.

--- include/libport/unistd.h
+++ include/libport/unistd.h
@@ -11,6 +11,13 @@
 #  include <unistd.h>
 # endif
 
+// OSX does not have O_LARGEFILE.  No information was found whether
+// some equivalent file is needed or not.  Other projects simply do as
+// follows.
+# ifndef O_LARGEFILE
+#  define O_LARGEFILE 0
+# endif
+
 // This seems to be its WIN32 equivalent.
 // http://msdn2.microsoft.com/en-us/library/ms811896(d=printer).aspx#ucmgch09_topic7.
 # if defined WIN32 || defined LIBPORT_WIN32
@@ -21,7 +28,7 @@
 #  if defined LIBPORT_HAVE__GETCWD
 #   define getcwd _getcwd
 #  elif defined LIBPORT_URBI_ENV_AIBO
-    // Will be defined in libport/unist.cc.
+    // Will be defined in libport/unistd.cc.
 #  else
 #   error I need either getcwd() or _getcwd()
 #  endif
