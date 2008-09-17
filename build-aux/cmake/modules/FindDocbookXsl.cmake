2a09f61fa874c1f5af026e9634148258f190e489
Vincent Ordy <ordy@gostai.com>
2008-09-17 20:29:08 +0200

    Add cmake modules for Docbook.
    	* build-aux/cmake/modules/FindDocbookXsl.cmake: New.
    	* build-aux/cmake/modules/Symlink.cmake: New.
    	* build-aux/cmake/modules/project-info.docbook.in: New.
    
    	* build-aux/cmake/modules/project-info.texi.in: Fix variable names.
    
    Signed-off-by: Vincent Ordy <ordy@gostai.com>

---
+++ build-aux/cmake/modules/FindDocbookXsl.cmake
@@ -0,0 +1,44 @@
+##
+## FindDocbookXsl.cmake: This file is part of cmake-aux.
+## Copyright (C) Gostai S.A.S., 2008.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+## For comments, bug reports and feedback: http://www.urbiforge.com
+##
+
+set(XSL_SEARCH_PATHS
+    # Fedora
+    "/usr/share/sgml/docbook/xsl-stylesheets"
+    # Cygwin
+    "/usr/share/docbook-xsl"
+    # Debian
+    "/usr/share/xml/docbook/stylesheet/nwalsh"
+    # SUSE
+    "/usr/share/xml/docbook/stylesheet/nwalsh/current"
+    # FreeBSD
+    "/usr/local/share/xsl/docbook"
+    # Gentoo
+    "/usr/share/sgml/docbook/xsl-stylesheets-*"
+    # Mac OS X + fink
+    "/sw/share/xml/xsl/docbook-xsl"
+    # Mac OS X + Mac ports
+    "/opt/local/share/xsl/docbook-xsl"
+    )
+
+if(NOT XSL_DIR)
+  find_path(XSL_DIR
+    NAMES catalog.xml
+    PATHS ${XSL_SEARCH_PATHS}
+    NO_DEFAULT_PATH)
+  if(XSL_DIR)
+    message(STATUS "Found Docbook XSL directory: "
+      "${XSL_DIR}")
+  else(XSL_DIR)
+    message(SEND_ERROR "no Docbook XSL found "
+      "(setting -DXSL_DIR may solve this problem).")
+  endif(XSL_DIR)
+endif(NOT XSL_DIR)
---
+++ build-aux/cmake/modules/Symlink.cmake
@@ -0,0 +1,39 @@
+##
+## Symlink.cmake: This file is part of libport.
+## Copyright (C) Gostai S.A.S., 2008.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+## For comments, bug reports and feedback: http://www.urbiforge.com
+##
+
+## Create a symlink of TARGET in DIRECTORY.
+## For Windows, copy instead of symlinking.
+
+if(NOT COMMAND SYMLINK)
+
+  function(SYMLINK TARGET DIRECTORY)
+    if(UNIX)
+      execute_process(COMMAND ${CMAKE_COMMAND}
+        -E create_symlink
+        "${TARGET}" "${DIRECTORY}"
+        )
+    else(UNIX)
+      if(IS_DIRECTORY)
+        execute_process(COMMAND ${CMAKE_COMMAND}
+          -E copy_directory
+          "${TARGET}" "${DIRECTORY}"
+          )
+      else(IS_DIRECTORY)
+        execute_process(COMMAND ${CMAKE_COMMAND}
+          -E copy
+          "${TARGET}" "${DIRECTORY}"
+          )
+      endif(IS_DIRECTORY)
+    endif(UNIX)
+  endfunction(SYMLINK)
+
+endif(NOT COMMAND SYMLINK)
