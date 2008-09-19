6e693e8f894f496ca9657b158534493075c56ac5
Vincent Ordy <ordy@gostai.com>
2008-09-19 14:19:32 +0200

    Fix Symlink.
    	* build-aux/cmake/modules/Symlink.cmake: here.
    
    Signed-off-by: Vincent Ordy <ordy@gostai.com>

--- build-aux/cmake/modules/Symlink.cmake
+++ build-aux/cmake/modules/Symlink.cmake
@@ -10,34 +10,65 @@
 ## For comments, bug reports and feedback: http://www.urbiforge.com
 ##
 
-## Create a symlink of TARGET in DIRECTORY.
+## Create a symlink of OLD in NEW.
 ## For Windows, copy instead of symlinking.
 
 if(NOT COMMAND SYMLINK)
 
-  function(SYMLINK TARGET DIRECTORY)
-    if(IS_DIRECTORY ${TARGET})
-      set(TARGET "${TARGET}/")
-    endif(IS_DIRECTORY ${TARGET})
-    string(REGEX REPLACE "/+$" "" DIRECTORY ${DIRECTORY})
+  function(SYMLINK OLD NEW)
+    ## Prepare
+    ## -------
+    ## cmake create_symlink does not support syntax "à la" 
+    ## `ln [-fs] source_file ... target_dir',
+    ## so we emulate it.
+
+    if(IS_DIRECTORY ${OLD})
+      string(REGEX REPLACE "/+$" "" OLD ${OLD})
+      set(OLD "${OLD}/")
+    endif(IS_DIRECTORY ${OLD})
+    if(NEW MATCHES "/$")
+      if(IS_DIRECTORY ${OLD})
+        string(REGEX MATCH "/[^/]+/$" FILENAME ${OLD})
+      else(IS_DIRECTORY ${OLD})
+        string(REGEX MATCH "/[^/]+$" FILENAME ${OLD})
+      endif(IS_DIRECTORY ${OLD})
+      if(FILENAME STREQUAL "")
+        message(SEND_ERROR "Failed to extract filename from ${OLD}")
+      endif(FILENAME STREQUAL "")
+      string(REPLACE "/" "" FILENAME ${FILENAME})
+      set(NEW "${NEW}/${FILENAME}")
+    endif(NEW MATCHES "/$")
+    if(EXISTS ${NEW})
+      message("-- Skipping symlink `${OLD}' to `${NEW}': ${NEW} already exists")
+      return()
+    endif(EXISTS ${NEW})
+    message("-- *** Symlinking `${OLD}' to `${NEW}'")
+
+    ## Do
     if(UNIX)
       execute_process(COMMAND ${CMAKE_COMMAND}
         -E create_symlink
-        "${TARGET}" "${DIRECTORY}"
+        "${OLD}" "${NEW}"
         )
     else(UNIX)
-      if(IS_DIRECTORY ${TARGET})
+      if(IS_DIRECTORY ${OLD})
         execute_process(COMMAND ${CMAKE_COMMAND}
           -E copy_directory
-          "${TARGET}" "${DIRECTORY}"
+          "${OLD}" "${NEW}"
           )
       else(IS_DIRECTORY)
         execute_process(COMMAND ${CMAKE_COMMAND}
           -E copy
-          "${TARGET}" "${DIRECTORY}"
+          "${OLD}" "${NEW}"
           )
       endif(IS_DIRECTORY)
     endif(UNIX)
+
+    ## Check
+    if(NOT EXISTS ${NEW})
+      message(SEND_ERROR "!!! Symlink creating failed!")
+    endif(NOT EXISTS ${NEW})
+
   endfunction(SYMLINK)
 
 endif(NOT COMMAND SYMLINK)
