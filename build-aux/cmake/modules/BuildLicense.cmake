92e7fee06aeeb3606ad5300be044395a3425907b
Nicolas Despres <despres@gostai.com>
2008-09-19 16:51:16 +0200

    Workaround unrtf shit.
    	* build-aux/cmake/modules/BuildLicense.cmake: Do that.
    
    Signed-off-by: Nicolas Despres <despres@gostai.com>

--- build-aux/cmake/modules/BuildLicense.cmake
+++ build-aux/cmake/modules/BuildLicense.cmake
@@ -8,7 +8,16 @@ include(UnrtfFile)
 
 function(BUILD_LICENSE input output)
   if(UNIX AND NOT CYGWIN AND NOT APPLE)
-    unrtf_file(${input} ${output} --text)
+    set(tmpout ${output}-workaround-unrtf.tmp)
+    unrtf_file(${input} ${tmpout} --text)
+    file(READ ${tmpout} buf)
+    #FIXME: unrtf introduces ` that confuse shitty dash shell by default on
+    #       all Ubuntu distro. So we substitue all ` by '.
+    string(REGEX REPLACE "`" "'" buf ${buf})
+    #FIXME: We also replace stupid ? introduces by unrtf where " is prefered.
+    string(REGEX REPLACE "\\?" "\"" buf ${buf})
+    file(WRITE ${output} ${buf})
+    file(REMOVE ${tmpout})
   else()
     # Simple copy.
     configure_file(${input} ${output} COPYONLY)
