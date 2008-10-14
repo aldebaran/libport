9412eed350fc19e13047ef9193d56c3d74085d75
Nicolas Despres <despres@gostai.com>
2008-10-14 15:17:20 +0200

    Allow to import already built documentation.
    	* build-aux/cmake/modules/Gostai.cmake: Do that.
    
    Signed-off-by: Nicolas Despres <despres@gostai.com>

--- build-aux/cmake/modules/Gostai.cmake
+++ build-aux/cmake/modules/Gostai.cmake
@@ -142,6 +142,9 @@ endmacro(gostai_add_executable)
 #  SOURCES	- the list of all XML files involved in the docbook.
 #  EXTRA	- the list of all extra files (others than generated HTML
 #		  files) Qt assistant requires (images for instance).
+#  IMPORT_BUILD - the path where to find already built file. This is useful
+#                 when your building the package on a platform that do not
+#		  support the tool chain needed to build the documentation.
 function(gostai_add_qt_assistant name)
 
   set(assistant_dir ${CMAKE_MODULE_PATH}/assistant)
@@ -149,172 +152,207 @@ function(gostai_add_qt_assistant name)
   set(css_filename styles.css)
   set(chunk_dir ${name}_assistant)
 
-  # Find tools.
-  find_package(XSLTProc REQUIRED)
-  find_package(QHelpGenerator REQUIRED)
-  find_package(QCollectionGenerator REQUIRED)
+  # Find tools
   find_package(QtAssistant REQUIRED)
 
   # Parse arguments.
   set(prefix ${name}_assistant)
   parse_arguments(
     ${prefix}
-    "SOURCES;EXTRA"
+    "SOURCES;EXTRA;IMPORT_BUILD"
     ""
     ${ARGN})
   set(${prefix}_SOURCES ${${prefix}_SOURCES} PARENT_SCOPE)
   set(${prefix}_EXTRA ${${prefix}_EXTRA} PARENT_SCOPE)
-
-  print_var(${prefix}_SOURCES ${prefix}_EXTRA)
-
-  # Search for the main file.
-  set(${prefix}_MAIN_SOURCE ${name}.xml)
-  set(${prefix}_MAIN_SOURCE ${${prefix}_MAIN_SOURCE} PARENT_SCOPE)
-  list(FIND ${prefix}_SOURCES ${${prefix}_MAIN_SOURCE} main_idx)
-  if(main_idx STREQUAL -1)
-    message(FATAL_ERROR
-      "cannot find assistant docbook main file '${${prefix}_MAIN_SOURCE}'")
-  endif(main_idx STREQUAL -1)
-
-  # Configure extra files style sheet.
-  set(extra_files ${css_filename} index.html)
-  list(APPEND extra_files ${${prefix}_EXTRA})
-  foreach(extra_file ${extra_files})
-    set(extra_files_xml
-      "${extra_files_xml}&lt;file&gt;${chunk_dir}/${extra_file}&lt;/file&gt;")
-  endforeach(extra_file)
-  configure_file(${stylesheets_dir}/extra_files.xsl.in
-    ${CMAKE_CURRENT_BINARY_DIR}/extra_files.xsl)
-
-  # Copy css.
-  configure_file(${assistant_dir}/${css_filename}
-    ${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/${css_filename}
-    COPYONLY)
-
-  # Copy extra files.
-  foreach(extra_file ${${prefix}_EXTRA})
-    configure_file(
-      ${extra_file}
-      ${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/${extra_file}
-      COPYONLY)
-  endforeach(extra_file)
-
-  # Configure project information.
-  configure_file(
-    ${CMAKE_MODULE_PATH}/project-info.docbook.in
-    ${CMAKE_CURRENT_BINARY_DIR}/project-info.xml)
-  # Configure Gostai information.
-  configure_file(
-    ${CMAKE_MODULE_PATH}/gostai-info.docbook.in
-    ${CMAKE_CURRENT_BINARY_DIR}/gostai-info.xml)
-
-  set(xsltproc_path
-    "${CMAKE_CURRENT_BINARY_DIR}")
-
-  # Compile chunk HTML version.
-  set(chunk_index ${chunk_dir}/index.html)
-  add_custom_command(
-    OUTPUT ${chunk_index}
-    COMMAND ${XSLTPROC_EXECUTABLE}
-
-    ARGS
-    --xinclude
-    --output ${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/
-    --path ${xsltproc_path}
-    ${stylesheets_dir}/chunk-stylesheet.xsl
-    ${${prefix}_MAIN_SOURCE}
-
-    DEPENDS
-    ${${prefix}_SOURCES}
-    ${stylesheets_dir}/chunk-stylesheet.xsl
-
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-
-  # Compile single page HTML version (not really maintained!)
-  set(html_filename ${name}.html)
-  add_custom_command(
-    OUTPUT ${html_filename}
-    COMMAND ${XSLTPROC_EXECUTABLE}
-
-    ARGS --xinclude
-    --output ${CMAKE_CURRENT_BINARY_DIR}/${html_filename}
-    --path ${xsltproc_path}
-    ${stylesheets_dir}/html-stylesheet.xsl
-    ${${prefix}_MAIN_SOURCE}
-
-    DEPENDS
-    ${${prefix}_SOURCES}
-    ${stylesheets_dir}/html-stylesheet.xsl
-
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-
-  # Compile Qt Help project (.qhp)
-  set(project_help_filename ${name}.qhp)
-  add_custom_command(
-    OUTPUT ${project_help_filename}
-    COMMAND ${XSLTPROC_EXECUTABLE}
-
-    ARGS
-    --xinclude
-    --output ${CMAKE_CURRENT_BINARY_DIR}/${project_help_filename}
-    --path ${xsltproc_path}
-    ${stylesheets_dir}/assistant-stylesheet.xsl
-    ${${prefix}_MAIN_SOURCE}
-
-    DEPENDS
-    ${${prefix}_SOURCES}
-    ${stylesheets_dir}/assistant-stylesheet.xsl
-    ${CMAKE_CURRENT_BINARY_DIR}/extra_files.xsl
-
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-
-  # Compile Qt Compressed help file. (.qch)
   set(compressed_help_name ${name}.qch)
-  set(compressed_help_filename
-    ${CMAKE_CURRENT_BINARY_DIR}/${compressed_help_name})
-  add_custom_command(
-    OUTPUT ${compressed_help_filename}
-    COMMAND ${QHELPGENERATOR_EXECUTABLE}
-    ARGS ${project_help_filename} -o ${compressed_help_filename}
-    DEPENDS ${project_help_filename} ${QHELPGENERATOR_EXECUTABLE}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
 
-  # Configure Qt Help Collection Project
-  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(ADDR_BAR_ENABLED "true")
-  else(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(ADDR_BAR_ENABLED "false")
-  endif(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set(REGISTER_SECTION "<file>${compressed_help_name}</file>")
-  set(collection_project_filename ${CMAKE_CURRENT_BINARY_DIR}/${name}.qhcp)
-  configure_file(${assistant_dir}/collection-help-project.qhcp.in
-    ${collection_project_filename}
-    ESCAPE_QUOTES
-    @ONLY
-    )
+  # We import already built documentation.
+  if(${prefix}_IMPORT_BUILD)
 
-  # Make Qt Collection file
-  configure_file(${assistant_dir}/gostai-doc.png
-    ${CMAKE_CURRENT_BINARY_DIR}/gostai-doc.png
-    COPYONLY
-    )
-  set(collection_filename ${CMAKE_CURRENT_BINARY_DIR}/${name}.qhc)
-  add_custom_command(
-    OUTPUT ${collection_filename}
-    COMMAND ${QCOLLECTIONGENERATOR_EXECUTABLE}
-    ARGS ${collection_project_filename} -o ${collection_filename}
+    if(EXISTS ${${prefix}_IMPORT_BUILD})
 
-    DEPENDS
-    ${collection_project_filename}
-    ${QCOLLECTIONGENERATOR_EXECUTABLE}
-    ${compressed_help_filename}
+      set(collection_filename ${${prefix}_IMPORT_BUILD}/${name}.qhc)
+      set(compressed_help_filename
+	${${prefix}_IMPORT_BUILD}/${compressed_help_name})
 
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
+    else(EXISTS ${${prefix}_IMPORT_BUILD})
+
+      message(SEND_ERROR
+	"documentation import directory '${${prefix}_IMPORT_BUILD}' does "
+	"not exists")
+
+    endif(EXISTS ${${prefix}_IMPORT_BUILD})
+
+  # otherwise we compile it from source
+  else(${prefix}_IMPORT_BUILD)
+
+    # Find tools.
+    find_package(XSLTProc REQUIRED)
+    find_package(QHelpGenerator REQUIRED)
+    find_package(QCollectionGenerator REQUIRED)
+
+    # Search for the main file.
+    set(${prefix}_MAIN_SOURCE ${name}.xml)
+    set(${prefix}_MAIN_SOURCE ${${prefix}_MAIN_SOURCE} PARENT_SCOPE)
+    list(FIND ${prefix}_SOURCES ${${prefix}_MAIN_SOURCE} main_idx)
+    if(main_idx STREQUAL -1)
+      message(FATAL_ERROR
+	"cannot find assistant docbook main file '${${prefix}_MAIN_SOURCE}'")
+    endif(main_idx STREQUAL -1)
+
+    # Configure extra files style sheet.
+    set(extra_files ${css_filename} index.html)
+    list(APPEND extra_files ${${prefix}_EXTRA})
+    foreach(extra_file ${extra_files})
+      set(extra_files_xml
+	"${extra_files_xml}&lt;file&gt;${chunk_dir}/${extra_file}&lt;/file&gt;")
+    endforeach(extra_file)
+    configure_file(${stylesheets_dir}/extra_files.xsl.in
+      ${CMAKE_CURRENT_BINARY_DIR}/extra_files.xsl)
+
+    # Copy css.
+    configure_file(${assistant_dir}/${css_filename}
+      ${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/${css_filename}
+      COPYONLY)
+
+    # Copy extra files.
+    foreach(extra_file ${${prefix}_EXTRA})
+      configure_file(
+	${extra_file}
+	${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/${extra_file}
+	COPYONLY)
+    endforeach(extra_file)
+
+    # Configure project information.
+    configure_file(
+      ${CMAKE_MODULE_PATH}/project-info.docbook.in
+      ${CMAKE_CURRENT_BINARY_DIR}/project-info.xml)
+    # Configure Gostai information.
+    configure_file(
+      ${CMAKE_MODULE_PATH}/gostai-info.docbook.in
+      ${CMAKE_CURRENT_BINARY_DIR}/gostai-info.xml)
+
+    set(xsltproc_path
+      "${CMAKE_CURRENT_BINARY_DIR}")
+
+    # Compile chunk HTML version.
+    set(chunk_index ${chunk_dir}/index.html)
+    add_custom_command(
+      OUTPUT ${chunk_index}
+      COMMAND ${XSLTPROC_EXECUTABLE}
+
+      ARGS
+      --xinclude
+      --output ${CMAKE_CURRENT_BINARY_DIR}/${chunk_dir}/
+      --path ${xsltproc_path}
+      ${stylesheets_dir}/chunk-stylesheet.xsl
+      ${${prefix}_MAIN_SOURCE}
+
+      DEPENDS
+      ${${prefix}_SOURCES}
+      ${stylesheets_dir}/chunk-stylesheet.xsl
+
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+
+    # Compile single page HTML version (not really maintained!)
+    set(html_filename ${name}.html)
+    add_custom_command(
+      OUTPUT ${html_filename}
+      COMMAND ${XSLTPROC_EXECUTABLE}
+
+      ARGS --xinclude
+      --output ${CMAKE_CURRENT_BINARY_DIR}/${html_filename}
+      --path ${xsltproc_path}
+      ${stylesheets_dir}/html-stylesheet.xsl
+      ${${prefix}_MAIN_SOURCE}
+
+      DEPENDS
+      ${${prefix}_SOURCES}
+      ${stylesheets_dir}/html-stylesheet.xsl
+
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+
+    # Compile Qt Help project (.qhp)
+    set(project_help_filename ${name}.qhp)
+    add_custom_command(
+      OUTPUT ${project_help_filename}
+      COMMAND ${XSLTPROC_EXECUTABLE}
+
+      ARGS
+      --xinclude
+      --output ${CMAKE_CURRENT_BINARY_DIR}/${project_help_filename}
+      --path ${xsltproc_path}
+      ${stylesheets_dir}/assistant-stylesheet.xsl
+      ${${prefix}_MAIN_SOURCE}
+
+      DEPENDS
+      ${${prefix}_SOURCES}
+      ${stylesheets_dir}/assistant-stylesheet.xsl
+      ${CMAKE_CURRENT_BINARY_DIR}/extra_files.xsl
+
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+
+    # Compile Qt Compressed help file. (.qch)
+    set(compressed_help_filename
+      ${CMAKE_CURRENT_BINARY_DIR}/${compressed_help_name})
+    add_custom_command(
+      OUTPUT ${compressed_help_filename}
+      COMMAND ${QHELPGENERATOR_EXECUTABLE}
+      ARGS ${project_help_filename} -o ${compressed_help_filename}
+      DEPENDS ${project_help_filename} ${QHELPGENERATOR_EXECUTABLE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      )
+
+    # Configure Qt Help Collection Project
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      set(ADDR_BAR_ENABLED "true")
+    else(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      set(ADDR_BAR_ENABLED "false")
+    endif(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(REGISTER_SECTION "<file>${compressed_help_name}</file>")
+    set(collection_project_filename ${CMAKE_CURRENT_BINARY_DIR}/${name}.qhcp)
+    configure_file(${assistant_dir}/collection-help-project.qhcp.in
+      ${collection_project_filename}
+      ESCAPE_QUOTES
+      @ONLY
+      )
+
+    # Make Qt Collection file
+    configure_file(${assistant_dir}/gostai-doc.png
+      ${CMAKE_CURRENT_BINARY_DIR}/gostai-doc.png
+      COPYONLY
+      )
+    set(collection_filename ${CMAKE_CURRENT_BINARY_DIR}/${name}.qhc)
+    add_custom_command(
+      OUTPUT ${collection_filename}
+      COMMAND ${QCOLLECTIONGENERATOR_EXECUTABLE}
+      ARGS ${collection_project_filename} -o ${collection_filename}
+
+      DEPENDS
+      ${collection_project_filename}
+      ${QCOLLECTIONGENERATOR_EXECUTABLE}
+      ${compressed_help_filename}
+
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      )
+
+    # Add associated target
+    add_custom_target(${prefix}
+      DEPENDS
+      ${chunk_index}
+      ${html_filename}
+      ${project_help_filename}
+      ${compressed_help_filename}
+      ${collection_filename}
+      )
+    if(TARGET doc)
+      add_dependencies(doc ${prefix})
+    endif(TARGET doc)
+
+  endif(${prefix}_IMPORT_BUILD)
 
   # Schedule for installation.
   install(
@@ -338,19 +376,6 @@ function(gostai_add_qt_assistant name)
     )
 
   dldep_install(${QT_QTASSISTANT_EXECUTABLE})
-
-  # Add associated target
-  add_custom_target(${prefix}
-    DEPENDS
-    ${chunk_index}
-    ${html_filename}
-    ${project_help_filename}
-    ${compressed_help_filename}
-    ${collection_filename}
-    )
-  if(TARGET doc)
-    add_dependencies(doc ${prefix})
-  endif(TARGET doc)
 
 endfunction(gostai_add_qt_assistant name)
 
