de6f39d1d36147184bf0101695aa84b133dcebb0
Nicolas Despres <despres@gostai.com>
2008-10-24 10:14:55 +0200

    Remove header.
    	* build-aux/cmake/modules/CPackInfo.cmake: Here.

--- build-aux/cmake/modules/CPackInfo.cmake
+++ build-aux/cmake/modules/CPackInfo.cmake
@@ -1,15 +1,3 @@
-##
-## CPackInfo.cmake: This file is part of libport.
-## Copyright (C) Gostai S.A.S., 2006-2008.
-##
-## This software is provided "as is" without warranty of any kind,
-## either expressed or implied, including but not limited to the
-## implied warranties of fitness for a particular purpose.
-##
-## See the LICENSE file for more information.
-## For comments, bug reports and feedback: http://www.urbiforge.com
-##
-
 # This module setup the variables used by cpack. It uses many variables already
 # set for package information. Here the list:
 #
