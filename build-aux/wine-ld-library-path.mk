3a370ab0d8207322553f6d353385b63e8da1fd9e
Akim Demaille <demaille@gostai.com>
2008-11-28 13:40:11 +0100

    Fix wine_ld_library_path expansion.
    	* build-aux/wine-ld-library-path.mk: Double expansion do not work
    	as expected.

--- build-aux/wine-ld-library-path.mk
+++ build-aux/wine-ld-library-path.mk
@@ -1,21 +1,21 @@
-# Wine is not providing easy means to pass LD_LIBRARY_PATH, which is a
-# problem especially for not-yet-installed libraries and executables.
-#
-# The script wine-ld-library-path implements the needed hack.  It
-# should be called with its arguments given as usual on the command
-# line, or in the envvar WINE_LD_LIBRARY_PATH_ARGS.  The point of this
-# envvar is to allow not to use the wrapper at all if it is not
-# needed.
+## Wine is not providing easy means to pass LD_LIBRARY_PATH, which is a
+## problem especially for not-yet-installed libraries and executables.
+##
+## The script wine-ld-library-path implements the needed hack.  It
+## should be called with its arguments given as usual on the command
+## line, or in the envvar WINE_LD_LIBRARY_PATH_ARGS.  The point of this
+## envvar is to allow not to use the wrapper at all if it is not
+## needed.
 
 wine_ld_library_path = $(build_aux_builddir)/wine-ld-library-path
 EXTRA_DIST += $(build_aux_dir)/wine-ld-library-path.as
 nodist_check_SCRIPTS += $(wine_ld_library_path)
 m4sh_scripts += $(wine_ld_library_path)
 
-RUN_WINE_LD_LIBRARY_PATH :=						\
-  $(shell								\
-     case $(host) in							\
-       (*pw32*|*mingw32*) echo '$$(shell $$(wine_ld_library_path))';;	\
+RUN_WINE_LD_LIBRARY_PATH :=					\
+  $(shell							\
+     case $(host) in						\
+       (*pw32*|*mingw32*) echo '$(wine_ld_library_path)';;	\
      esac)
 
 clean-local: clean-wine-ld-library-path
@@ -27,4 +27,5 @@ clean-wine-ld-library-path:
 debug: debug-wine-ld-library-path
 .PHONY: debug-wine-ld-library-path
 debug-wine-ld-library-path:
-	echo '$(RUN_WINE_LD_LIBRARY_PATH)'
+	@echo 'RUN_WINE_LD_LIBRARY_PATH = $(RUN_WINE_LD_LIBRARY_PATH)'
+	@echo 'TESTS_ENVIRONMENT = $(TESTS_ENVIRONMENT)'
