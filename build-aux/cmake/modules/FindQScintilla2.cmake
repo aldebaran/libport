a6f47f6fe8d494af241fbf29efb8c0deaa31fd37
Nicolas Despres <despres@gostai.com>
2008-12-18 18:06:31 +0100

    Add support for debug and release mode.
    	* build-aux/cmake/modules/FindQScintilla2.cmake: Here.

--- build-aux/cmake/modules/FindQScintilla2.cmake
+++ build-aux/cmake/modules/FindQScintilla2.cmake
@@ -17,46 +17,65 @@
 # This module set two variables:
 #
 #   QSCINTILLA2_LIBRARY: the path to the libraries found.
+#   QSCINTILLA2_LIBRARY_RELEASE: the path to the release libraries found.
+#   QSCINTILLA2_LIBRARY_DEBUG: the path to the debug libraries found.
 #   QSCINTILLA2_INCLUDE_DIR: the include path.
 #
 # These variables are set to <var-name>-NOTFOUND if an error occurs.
 
-set(QSCINTILLA2_FOUND FALSE)
-
-# Search for the library.
 if(NOT QSCINTILLA2_LIBRARY)
-  find_library(QSCINTILLA2_LIBRARY
-    NAMES qscintilla2
-    PATHS ${QT_LIBRARY_DIR})
-  if(QSCINTILLA2_LIBRARY)
-    message(STATUS "Found qscintilla2 library: ${QSCINTILLA2_LIBRARY}")
-    set(QSCINTILLA2_FOUND TRUE)
-  else(QSCINTILLA2_LIBRARY)
-    message(SEND_ERROR "no qscintilla2 library found "
-      "(setting -DQSCINTILLA2_ROOT_DIR=/path/to/qscintilla2 may solve this "
-      "problem).")
-  endif(QSCINTILLA2_LIBRARY)
+
+  # Release
+  if(NOT QSCINTILLA2_LIBRARY_RELEASE)
+    find_library(QSCINTILLA2_LIBRARY_RELEASE
+      NAMES qscintilla2
+      PATHS ${QT_LIBRARY_DIR})
+    if(QSCINTILLA2_LIBRARY_RELEASE)
+      message(STATUS "Found qscintilla2 release library: ${QSCINTILLA2_LIBRARY_RELEASE}")
+    endif(QSCINTILLA2_LIBRARY_RELEASE)
+  endif(NOT QSCINTILLA2_LIBRARY_RELEASE)
+  # Debug
+  if(NOT QSCINTILLA2_LIBRARY_DEBUG)
+    find_library(QSCINTILLA2_LIBRARY_DEBUG
+      NAMES qscintilla2d
+      PATHS ${QT_LIBRARY_DIR})
+    if(QSCINTILLA2_LIBRARY_DEBUG)
+      message(STATUS "Found qscintilla2 debug library: ${QSCINTILLA2_LIBRARY_DEBUG}")
+    endif(QSCINTILLA2_LIBRARY_DEBUG)
+  endif(NOT QSCINTILLA2_LIBRARY_DEBUG)
+
+  if(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    if(QSCINTILLA2_LIBRARY_RELEASE OR QSCINTILLA2_LIBRARY_DEBUG)
+      set(QSCINTILLA2_LIBRARY
+	optimized
+	${QSCINTILLA2_LIBRARY_RELEASE}
+	debug
+	${QSCINTILLA2_LIBRARY_DEBUG}
+	)
+    endif(QSCINTILLA2_LIBRARY_RELEASE OR QSCINTILLA2_LIBRARY_DEBUG)
+  else(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    set(QSCINTILLA2_LIBRARY ${QSCINTILLA2_LIBRARY_RELEASE})
+  endif(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+
 endif(NOT QSCINTILLA2_LIBRARY)
 
-if(QSCINTILLA2_FOUND)
-  # Search for the include directory.
-  if(NOT QSCINTILLA2_INCLUDE_DIR)
-    find_path(QSCINTILLA2_INCLUDE_DIR
-      NAMES Qsci/qsciapis.h
-      PATHS ${QT_INCLUDE_DIR})
-    if(QSCINTILLA2_INCLUDE_DIR)
-      message(STATUS "Found qscintilla2 headers directory: "
-	"${QSCINTILLA2_INCLUDE_DIR}")
-    else(QSCINTILLA2_INCLUDE_DIR)
-      message(SEND_ERROR "no qscintilla2 headers found "
-	"(setting -DQSCINTILLA2_ROOT_DIR=/path/to/qscintilla2 may solve this "
-	"problem).")
-      set(QSCINTILLA2_FOUND FALSE)
-    endif(QSCINTILLA2_INCLUDE_DIR)
-  endif(NOT QSCINTILLA2_INCLUDE_DIR)
-endif(QSCINTILLA2_FOUND)
+# Search for the include directory.
+if(NOT QSCINTILLA2_INCLUDE_DIR)
+  find_path(QSCINTILLA2_INCLUDE_DIR
+    NAMES Qsci/qsciapis.h
+    PATHS ${QT_INCLUDE_DIR})
+  if(QSCINTILLA2_INCLUDE_DIR)
+    message(STATUS "Found qscintilla2 headers directory: "
+      "${QSCINTILLA2_INCLUDE_DIR}")
+  else(QSCINTILLA2_INCLUDE_DIR)
+    message(SEND_ERROR "no qscintilla2 headers found "
+      "(setting -DQSCINTILLA2_ROOT_DIR=/path/to/qscintilla2 may solve this "
+      "problem).")
+    set(QSCINTILLA2_FOUND FALSE)
+  endif(QSCINTILLA2_INCLUDE_DIR)
+endif(NOT QSCINTILLA2_INCLUDE_DIR)
 
-if(QSCINTILLA2_FOUND AND QSCINTILLA2_INCLUDE_DIR)
+if(NOT QSCINTILLA2_SCINTILLA_INCLUDE_DIR)
   # Check whether scintilla header are installed (needed because we write our
   # own lexer).
   find_path(QSCINTILLA2_SCINTILLA_INCLUDE_DIR
@@ -65,9 +84,8 @@ if(QSCINTILLA2_FOUND AND QSCINTILLA2_INCLUDE_DIR)
   if(QSCINTILLA2_SCINTILLA_INCLUDE_DIR)
     message(STATUS "Found scintilla header directory: "
       "${QSCINTILLA2_SCINTILLA_INCLUDE_DIR}")
-  else()
+  else(QSCINTILLA2_SCINTILLA_INCLUDE_DIR)
     message(SEND_ERROR "no scintilla headers found "
       "(check that your apply our patch before to install QScintilla).")
-    set(QSCINTILLA2_FOUND FALSE)
-  endif()
-endif()
+  endif(QSCINTILLA2_SCINTILLA_INCLUDE_DIR)
+endif(NOT QSCINTILLA2_SCINTILLA_INCLUDE_DIR)
