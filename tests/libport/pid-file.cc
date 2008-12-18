8a862dc478c504c67e306dfd8eb166a0ddef14c2
Akim Demaille <demaille@gostai.com>
2008-12-18 15:47:04 +0100

    Fix pid-file.
    	* tests/libport/Makefile.am (TESTSFLAGS): New.
    	Use it.
    	* tests/libport/pid-file.cc: Remove useless includes.
    	Fix precedence bw = and !=.

--- tests/libport/pid-file.cc
+++ tests/libport/pid-file.cc
@@ -1,7 +1,5 @@
 #include <fstream>
 
-#include <boost/format.hpp>
-
 #include <libport/pid-file.hh>
 #include <libport/program-name.hh>
 #include <libport/unit-test.hh>
@@ -52,7 +50,7 @@ void test_exists()
   static const std::string file = "exists.pid";
   // Create the file
   int fd;
-  BOOST_CHECK(fd = open(file.c_str(), O_CREAT) != -1);
+  BOOST_CHECK_NE(fd = open(file.c_str(), O_CREAT), -1);
   BOOST_CHECK(!close(fd));
 
   libport::PidFile f(file);
