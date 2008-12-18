45d4b8fa2ddb128e19313afbe8acc8d00d8e592c
Akim Demaille <demaille@gostai.com>
2008-12-18 16:03:00 +0100

    Convert statistics test to Boost Unit test.
    Hoping this will help to understand Windows issues more easily.
    
    	* tests/libport/statistics.cc: here.

--- tests/libport/statistics.cc
+++ tests/libport/statistics.cc
@@ -1,7 +1,11 @@
-#include <cassert>
-#include <libport/statistics.hh>
-
 #include <iostream>
+#include <boost/bind.hpp>
+#include <libport/unit-test.hh>
+
+using boost::bind;
+using libport::test_suite;
+
+#include <libport/statistics.hh>
 
 // Visual C++ gets confused if we use "fabs" in the template below, as
 // it is an overloaded operator.
@@ -18,16 +22,18 @@ check(const libport::Statistics<T, R>& s,
 {
   std::cerr << "Mean: " << s.mean() << std::endl;
   std::cerr << "Variance: " << s.variance() << std::endl;
-  assert(size == s.size());
-  assert(min == s.min());
-  assert(max == s.max());
-  assert(_fabs(mean - s.mean()) < 1e-4);
-  assert(_fabs(variance - s.variance()) < 1e-4);
+  BOOST_CHECK_EQUAL(size, s.size());
+  BOOST_CHECK_EQUAL(min, s.min());
+  BOOST_CHECK_EQUAL(max, s.max());
+  BOOST_CHECK_LT(_fabs(mean - s.mean()), 1e-4);
+  BOOST_CHECK_LT(_fabs(variance - s.variance()), 1e-4);
 }
 
 template<typename R>
 static void
-check1(size_t capacity, unsigned int size, unsigned int min, unsigned int max, R mean, R variance)
+check1(size_t capacity, unsigned int size,
+       unsigned int min, unsigned int max,
+       R mean, R variance)
 {
   libport::Statistics<unsigned int, R> s(capacity);
   s.add_sample(2);
@@ -38,18 +44,24 @@ check1(size_t capacity, unsigned int size, unsigned int min, unsigned int max, R
   check(s, size, min, max, mean, variance);
 }
 
-int
-main()
+test_suite*
+init_test_suite()
 {
+  test_suite* suite = BOOST_TEST_SUITE("libport::statistics");
+
+#define TEST(Args)                                      \
+  suite->add(BOOST_TEST_CASE(boost::bind Args));
+
   // Running samples
-  check1<unsigned int>(3, 3, 3, 7, 4, 3);
-  check1<double>(3, 3, 3, 7, 4.666666666, 2.8888888888);
+  TEST((check1<unsigned int>, 3, 3, 3, 7, 4, 3));
+  TEST((check1<double>, 3, 3, 3, 7, 4.666666666, 2.8888888888));
 
   // Non-running samples
-  check1<unsigned int>(0, 5, 2, 7, 3, 3);
-  check1<double>(0, 5, 2, 7, 3.6, 3.44);
+  TEST((check1<unsigned int>, 0, 5, 2, 7, 3, 3));
+  TEST((check1<double>, 0, 5, 2, 7, 3.6, 3.44));
 
   // Running samples with non-full buffer
-  check1<unsigned int>(10, 5, 2, 7, 3, 3);
-  check1<double>(10, 5, 2, 7, 3.6, 3.44);
+  TEST((check1<unsigned int>, 10, 5, 2, 7, 3, 3));
+  TEST((check1<double>, 10, 5, 2, 7, 3.6, 3.44));
+  return suite;
 }
