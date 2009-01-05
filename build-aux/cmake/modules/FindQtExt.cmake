a12571c1285c3a4b5652eaa16b344bd14147def1
Clement Sommelet <sommelet@gostai.com>
2009-01-05 16:06:27 +0100

    FindQtExt: look for both debug and release versions of qt_ext library.
    	* build-aux/cmake/modules/FindQtExt.cmake: .

--- build-aux/cmake/modules/FindQtExt.cmake
+++ build-aux/cmake/modules/FindQtExt.cmake
@@ -25,16 +25,46 @@ include(QtExt)
 
 set(QT_EXT_FOUND FALSE)
 
-# Search for the library urbi.
+# Search for the library qt_ext.
+
 if(NOT QT_EXT_LIBRARY)
-  find_library(QT_EXT_LIBRARY qt_ext)
-  if(QT_EXT_LIBRARY)
-    message(STATUS "Found qt-ext library: ${QT_EXT_LIBRARY}")
-    set(QT_EXT_FOUND TRUE)
-  else(QT_EXT_LIBRARY)
-    message(SEND_ERROR "no qt-ext library found "
-      "(setting -DCMAKE_INCLUDE_PATH may solve this problem).")
-  endif(QT_EXT_LIBRARY)
+
+  #Release
+  if(NOT QT_EXT_LIBRARY_RELEASE)
+    find_library(QT_EXT_LIBRARY_RELEASE qt_ext)
+    if(QT_EXT_LIBRARY_RELEASE)
+      message(STATUS "Found qt-ext release library: ${QT_EXT_LIBRARY_RELEASE}")
+      set(QT_EXT_FOUND TRUE)
+    else(QT_EXT_LIBRARY_RELEASE)
+      message(SEND_ERROR "no qt-ext release library found "
+        "(setting -DCMAKE_INCLUDE_PATH may solve this problem).")
+    endif(QT_EXT_LIBRARY_RELEASE)
+  endif(NOT QT_EXT_LIBRARY_RELEASE)
+
+  #Debug
+  if(NOT QT_EXT_LIBRARY_DEBUG)
+    find_library(QT_EXT_LIBRARY_DEBUG qt_extd)
+    if(QT_EXT_LIBRARY_DEBUG)
+      message(STATUS "Found qt-extd debug library: ${QT_EXT_LIBRARY_DEBUG}")
+      set(QT_EXT_FOUND TRUE)
+    else(QT_EXT_LIBRARY_DEBUG)
+      message(SEND_ERROR "no qt-ext debug library found "
+        "(setting -DCMAKE_INCLUDE_PATH may solve this problem).")
+    endif(QT_EXT_LIBRARY_DEBUG)
+  endif(NOT QT_EXT_LIBRARY_DEBUG)
+
+  #Set QT_EXT_LIBRARY to the right value if needed
+  if(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    if(QT_EXT_LIBRARY_RELEASE OR QT_EXT_LIBRARY_DEBUG)
+      set(QT_EXT_LIBRARY
+	        optimized ${QT_EXT_LIBRARY_RELEASE}
+	        debug     ${QT_EXT_LIBRARY_DEBUG}
+      )
+    endif(QT_EXT_LIBRARY_RELEASE OR QT_EXT_LIBRARY_DEBUG)
+  else(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    set(QT_EXT_LIBRARY ${QT_EXT_LIBRARY_RELEASE})
+  endif(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+
 endif(NOT QT_EXT_LIBRARY)
 
 if(QT_EXT_FOUND)
