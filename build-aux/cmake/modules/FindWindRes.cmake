b0728132204830825ae7a79c3dbb79901d49388e
Nicolas Despres <despres@gostai.com>
2009-01-09 01:07:27 +0100

    Find cross version of windres.
    	* build-aux/cmake/modules/FindWindRes.cmake: Here.

--- build-aux/cmake/modules/FindWindRes.cmake
+++ build-aux/cmake/modules/FindWindRes.cmake
@@ -2,7 +2,7 @@
 
 if(NOT WINDRES_EXECUTABLE)
 
-  find_program(WINDRES_EXECUTABLE NAMES windres)
+  find_program(WINDRES_EXECUTABLE NAMES windres i386-mingw32-windres)
   if(NOT WINDRES_EXECUTABLE)
     message(SEND_ERROR "Cannot find 'windres' executable.")
   endif(NOT WINDRES_EXECUTABLE)
