f638cdf55c447df9e70739415fc2f5df49f951e2
Akim Demaille <demaille@gostai.com>
2009-01-16 13:47:12 +0100

    Check depcomp.
    	* msvc/depcomp.m4sh: New.
    	* msvc/local.mk: here.

--- msvc/local.mk
+++ msvc/local.mk
@@ -17,5 +17,13 @@ m4sh_scripts += msvc/compilation
 msvc/compilation.log: msvc/compilation
 	@$(am__check_pre) $${dir}$< $(am__check_post)
 
-MSVC_TESTS = msvc/compilation
+# depcomp
+EXTRA_DIST += msvc/depcomp.m4sh
+nodist_check_SCRIPTS += msvc/depcomp
+m4sh_scripts += msvc/depcomp
+
+msvc/depcomp.log: msvc/depcomp
+	@$(am__check_pre) $${dir}$< $(am__check_post)
+
+MSVC_TESTS = msvc/compilation msvc/depcomp
 TESTS += $(MSVC_TESTS)
