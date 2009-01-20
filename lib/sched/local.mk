43f37a46cff732da0ef0054e8c2c250e56e56670
Akim Demaille <demaille@gostai.com>
2009-01-20 14:43:57 +0100

    libsched depends on libport.
    	* lib/sched/local.mk (lib_sched_libsched_la_LIBADD): New.

--- lib/sched/local.mk
+++ lib/sched/local.mk
@@ -1,6 +1,7 @@
 lib_LTLIBRARIES += lib/sched/libsched.la
 dist_lib_sched_libsched_la_SOURCES = $(lib_sched_libsched_sources)
 lib_sched_libsched_la_LDFLAGS = -avoid-version -no-undefined
+lib_sched_libsched_la_LIBADD = $(LIBADD) lib/libport/libport.la
 lib_sched_libsched_la_CPPFLAGS =		\
   $(AM_CPPFLAGS)				\
   -I$(srcdir)/lib				\
