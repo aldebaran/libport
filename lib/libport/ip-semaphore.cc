efcf04d34bf54389a42961db15f72bb335d25b23
mefyl <hocquet@gostai.com>
2009-02-10 15:10:18 +0100

    Document IPSemaphore implementation.
    	* lib/libport/ip-semaphore.cc: Here.

--- lib/libport/ip-semaphore.cc
+++ lib/libport/ip-semaphore.cc
@@ -13,6 +13,7 @@ namespace libport
 {
   IPSemaphore::IPSemaphore(int count)
   {
+    // Create a set of 1 semaphore, with permisions rwx------
     id_ = semget(IPC_PRIVATE, 1, 0700);
 
     // The man specifies that this union must be defined by the
@@ -25,17 +26,22 @@ namespace libport
                                   (Linux-specific) */
     };
 
+    // Initialize its value to count
     semun arg;
     arg.val = count;
     semctl(id_, 0, SETVAL, arg);
   }
 
+  // Add \a val to semaphore \a id
   static void alter_sem(int id, int val)
   {
     assert(val != 0);
     sembuf sops[1];
     sops[0].sem_num = 0;
     sops[0].sem_op = val;
+    // If the process terminates, undo the change. This avoid blocking
+    // several processes if a process segv before it releases the
+    // semaphore, for instance.
     sops[0].sem_flg = SEM_UNDO;
     semop(id, sops, 1);
   }
