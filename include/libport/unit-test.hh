3d9a4e8da3f35143fb3bf57ba0baf565c84d47db
mefyl <hocquet@gostai.com>
2009-02-26 14:38:00 +0100

    Do not assume boost is dynamic.
    	* include/libport/unit-test.hh: Here: handle both cases.

--- include/libport/unit-test.hh
+++ include/libport/unit-test.hh
@@ -4,7 +4,12 @@
 // See documentation on:
 // https://core.gostai.com/projects/common/wiki/BoostUnit
 
-# define BOOST_TEST_DYN_LINK
+# ifndef BOOST_STATIC
+#  define BOOST_TEST_DYN_LINK
+# endif
+
+# include <boost/config.hpp>
+# include <boost/test/detail/config.hpp>
 # include <boost/test/unit_test.hpp>
 
 namespace libport
@@ -15,6 +20,16 @@ namespace libport
 // Fwd for the entry point function the user must implement
 libport::test_suite*
 init_test_suite();
+
+// FIXME: Boost unit-test has a different API depending on whether
+// it's static or dynamic (the first defines main, the later doesn't).
+// Boost defines BOOST_TEST_DYN_LINK to use the dynamic API, but
+// unfortunately it seems it's not available for us to test and switch
+// our own API accordingly. For now, we manually define BOOST_STATIC
+// on architectures where boost is static. Our m4 macro should define
+// it automagically.
+
+# ifndef BOOST_STATIC
 
 namespace libport
 {
@@ -31,5 +46,14 @@ main(int argc, char** argv)
   return ::boost::unit_test::unit_test_main(
     libport::_init_test_suite, argc, argv);
 }
+
+# else
+
+libport::test_suite* init_unit_test_suite(int, char**)
+{
+  return init_test_suite();
+}
+
+# endif
 
 #endif
