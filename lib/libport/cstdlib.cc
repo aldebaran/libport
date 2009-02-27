6b882557da55e634a47ec1477fb04ab155502aa4
mefyl <hocquet@gostai.com>
2009-02-27 11:12:52 +0100

    Define strtoll only for Visual C++.
    	* include/libport/cstdlib,
    	* lib/libport/cstdlib.cc: Here.

--- lib/libport/cstdlib.cc
+++ lib/libport/cstdlib.cc
@@ -1,18 +1,21 @@
 #include <libport/detect-win32.h>
-# ifdef MSVC
+#include <string>
+#include <libport/cstdlib>
 
-# include <string>
-# include <libport/cstdlib>
+#if defined MSVC || defined __MINGW32__
 
 extern "C"
 {
-
   int
   setenv(const char* key, const char* value, int overwrite)
   {
     if (!overwrite && getenv(key))
       return 0;
+#if defined __MINGW32__ // MinGW has no _putenv_s
+    return _putenv((std::string(key) + "=" + value).c_str());
+#else
     return _putenv_s(key, value);
+#endif
   }
 
   int
@@ -22,13 +25,19 @@ extern "C"
     // the envvar.
     return setenv(key, "", 1);
   }
+}
 
+#endif
+
+#ifdef MSVC
+
+extern "C"
+{
   long long
   strtoll(const char* nptr, char** endptr, int base)
   {
     return _strtoi64(nptr, endptr, base);
   }
-
 }
 
 #endif
