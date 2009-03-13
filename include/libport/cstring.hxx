48c4a481a910280ce9ff066bba239086794eccba
Akim Demaille <demaille@gostai.com>
2009-03-13 16:29:23 +0100

    strneq.
    	* include/libport/cstring, include/libport/cstring.hxx (strneq):
    	New.

--- include/libport/cstring.hxx
+++ include/libport/cstring.hxx
@@ -1,10 +1,16 @@
 namespace libport
 {
-  /// Whether the C string \a Lhs and \a Rhs are equal.
+  inline
   bool
   streq(const char* lhs, const char* rhs)
   {
     return !strcmp(lhs, rhs);
   }
-}
 
+  inline
+  bool
+  strneq(const char* lhs, const char* rhs, size_t n)
+  {
+    return !strncmp(lhs, rhs, n);
+  }
+}
