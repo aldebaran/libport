898d549eba78a53f2b51fe07a45d944dbca357ae
Simon Nivault <nivault@gostai.com>
2009-04-08 13:45:50 +0200

    semaphore: Add test case about waiting with timeout.
    	* tests/libport/semaphore.cc: here.

--- tests/libport/semaphore.cc
+++ tests/libport/semaphore.cc
@@ -1,5 +1,8 @@
+#include <libport/pthread.h>
 #include <libport/semaphore.hh>
 #include <libport/unit-test.hh>
+#include <libport/unistd.h>
+#include <libport/utime.hh>
 
 using libport::test_suite;
 
@@ -13,10 +16,35 @@ static void test_semaphore()
   BOOST_CHECK_EQUAL(0u, libport::Semaphore::instances());
 }
 
+void* thread1(void* data)
+{
+  libport::Semaphore* sem = (libport::Semaphore *)data;
+  sleep(2);
+  (*sem)++;
+  return 0;
+}
+
+void check_timeout()
+{
+  pthread_t th;
+  libport::Semaphore sem(1);
+  libport::utime_t t1, t2;
+
+  sem--;
+  t1 = libport::utime();
+  pthread_create(&th, 0, &thread1, &sem);
+  BOOST_CHECK_EQUAL(sem.get(1), false);
+  t2 = libport::utime() - t1;
+  BOOST_CHECK(t2 < 1010000);
+  pthread_join(th, NULL);
+  sem++;
+}
+
 test_suite*
 init_test_suite()
 {
   test_suite* suite = BOOST_TEST_SUITE("libport::semaphore");
   suite->add(BOOST_TEST_CASE(test_semaphore));
+  suite->add(BOOST_TEST_CASE(check_timeout));
   return suite;
 }
