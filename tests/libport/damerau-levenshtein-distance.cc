9158994c0d067e0c83595519b7bdb5b5468f5d88
Simon Nivault <nivault@gostai.com>
2009-05-18 12:00:25 +0200

    distance: add test and some formatting changes.
    	* tests/libport/damerau-levenshtein-distance.cc: New.
    	* tests/libport/local.mk: add file.

---
+++ tests/libport/damerau-levenshtein-distance.cc
@@ -0,0 +1,80 @@
+#include <libport/damerau-levenshtein-distance.hh>
+
+#include <libport/unit-test.hh>
+
+using libport::test_suite;
+
+void check_hard() // Not really checked
+{
+  std::string s1("In information theory and computer science,"
+		 " Damerau-Levenshtein distance is a \"distance\" "
+		 "(string metric) between two strings, i.e., finite sequence "
+		 "of symbols, given by counting the minimum number of "
+		 "operations needed to transform one string into the other, "
+		 "where an operation is defined as an insertion, deletion, or "
+		 "substitution of a single character, or a transposition of "
+		 "two characters. Damerau in his seminal paper not only "
+		 "distinguished these four edit operations but also stated "
+		 "that they correspond to more than 80% of all human "
+		 "misspellings. Damerau concentrated on single-character "
+		 "misspellings. Edit distance was introduced by Levenshtein, "
+		 "who generalized this concept with multiple edit operations, "
+		 "but did not include transpositions in the set of basic "
+		 "operations. While the original motivation was to measure "
+		 "distance between human misspellings to improve applications "
+		 "such as spell checkers, Damerau-Levenshtein distance has "
+		 "also seen uses in biology to measure the variation between "
+		 "DNA.");
+  std::string s2("The above-described pseudo-code calculates only restricted "
+		 "edit distance. Damerau?Levenshtein distance plays an "
+		 "important role in natural language processing. In natural "
+		 "languages, strings are short and the number of errors "
+		 "(misspellings) rarely exceeds 2. In such circumstances, "
+		 "restricted and real edit distance differ very rarely. That "
+		 "is why this limitation is not very important. However, one "
+		 "must remember that restricted edit distance does not always "
+		 "satisfy the triangle inequality and, thus, cannot be used "
+		 "with metric trees. An extension of the edit distance "
+		 "algorithm, that does satisfy the triangle inequality is "
+		 "described in the paper F.J. Damerau. A technique for "
+		 "computer detection and correction of spelling errors, "
+		 "Communications of the ACM, 1964");
+  for (int i = 0; i < 100; ++i)
+    libport::damerau_levenshtein_distance(s1, s2);
+}
+
+#define CHECK(s1, s2, d)						\
+  BOOST_CHECK_EQUAL(libport::damerau_levenshtein_distance(s1, s2), d)
+
+void check_deletion()
+{
+  CHECK("azertyuiop", "aeryuop", 3);
+}
+
+void check_insertion()
+{
+  CHECK("aeryuop", "azertyuiop", 3);
+}
+
+void check_substitution()
+{
+  CHECK("azertyuiopqsdfghjklmwxcvbn,", "qwertyuiopasdfghjkl;zxcvbnm", 6);
+}
+
+void check_transposition()
+{
+  CHECK("1234567890", "1324576809", 3);
+}
+
+test_suite*
+init_test_suite()
+{
+  test_suite* suite =
+    BOOST_TEST_SUITE("libport::damerau-levenshtein-distance");
+//  suite->add(BOOST_TEST_CASE(check_hard));
+  suite->add(BOOST_TEST_CASE(check_deletion));
+  suite->add(BOOST_TEST_CASE(check_insertion));
+  suite->add(BOOST_TEST_CASE(check_substitution));
+  suite->add(BOOST_TEST_CASE(check_transposition));
+  return suite;
+}
--- tests/libport/local.mk
+++ tests/libport/local.mk
@@ -6,52 +6,53 @@ TESTSFLAGS = --log_level=test-suite
 # using instrumentation with Boost.Test.
 #
 # INSTRUMENT = $(build_aux_srcdir)/instrument
-# INSTRUMENTFLAGS =							 \
+# INSTRUMENTFLAGS =                                                      \
 #   --valgrind-option=--suppressions=$(build_aux_srcdir)/instrument.supp \
-#   --valgrind-option=--gen-suppressions=all				 \
+#   --valgrind-option=--gen-suppressions=all                             \
 #   --libtool=$(abs_top_builddir)/libtool
 
 # For some reason, this rule *must* be defined before loading
 # check.mk.
 %.log: %$(EXEEXT)
-	@$(am__check_pre) ./$< $(TESTSFLAGS) $(am__check_post)
+        @$(am__check_pre) ./$< $(TESTSFLAGS) $(am__check_post)
 
 # Program to check:
-TESTS_BINARIES =				\
-  tests/libport/asio.cc				\
-  tests/libport/base64.cc			\
-  tests/libport/backtrace.cc			\
-  tests/libport/cli.cc				\
-  tests/libport/condition.cc			\
-  tests/libport/cstdlib.cc			\
-  tests/libport/debug.cc			\
-  tests/libport/deref.cc			\
-  tests/libport/dirent.cc			\
-  tests/libport/erase-if.cc			\
-  tests/libport/escape.cc			\
-  tests/libport/fd-stream.cc			\
-  tests/libport/fifo.cc				\
-  tests/libport/file-library.cc			\
-  tests/libport/finally.cc			\
-  tests/libport/foreach.cc			\
-  tests/libport/has-if.cc			\
-  tests/libport/indent.cc			\
-  tests/libport/intrusive-ptr.cc		\
-  tests/libport/io-stream.cc			\
-  tests/libport/markup-ostream.cc		\
-  tests/libport/path.cc				\
-  tests/libport/pid-file.cc			\
-  tests/libport/pthread.cc			\
-  tests/libport/semaphore.cc			\
-  tests/libport/separator.cc			\
-  tests/libport/singleton-ptr.cc		\
-  tests/libport/statistics.cc			\
-  tests/libport/symbol.cc			\
-  tests/libport/time.cc 			\
-  tests/libport/timer.cc			\
-  tests/libport/tokenizer.cc			\
-  tests/libport/ufloat-double.cc		\
-  tests/libport/unescape.cc			\
+TESTS_BINARIES =                                \
+  tests/libport/asio.cc                         \
+  tests/libport/base64.cc                       \
+  tests/libport/backtrace.cc                    \
+  tests/libport/cli.cc                          \
+  tests/libport/condition.cc                    \
+  tests/libport/cstdlib.cc                      \
+  tests/libport/damerau-levenshtein-distance.cc \
+  tests/libport/debug.cc                        \
+  tests/libport/deref.cc                        \
+  tests/libport/dirent.cc                       \
+  tests/libport/erase-if.cc                     \
+  tests/libport/escape.cc                       \
+  tests/libport/fd-stream.cc                    \
+  tests/libport/fifo.cc                         \
+  tests/libport/file-library.cc                 \
+  tests/libport/finally.cc                      \
+  tests/libport/foreach.cc                      \
+  tests/libport/has-if.cc                       \
+  tests/libport/indent.cc                       \
+  tests/libport/intrusive-ptr.cc                \
+  tests/libport/io-stream.cc                    \
+  tests/libport/markup-ostream.cc               \
+  tests/libport/path.cc                         \
+  tests/libport/pid-file.cc                     \
+  tests/libport/pthread.cc                      \
+  tests/libport/semaphore.cc                    \
+  tests/libport/separator.cc                    \
+  tests/libport/singleton-ptr.cc                \
+  tests/libport/statistics.cc                   \
+  tests/libport/symbol.cc                       \
+  tests/libport/time.cc                         \
+  tests/libport/timer.cc                        \
+  tests/libport/tokenizer.cc                    \
+  tests/libport/ufloat-double.cc                \
+  tests/libport/unescape.cc                     \
   tests/libport/utime.cc
 
 # ufloat-float.cc is commented out as we do not support ufloat /=
@@ -74,58 +75,59 @@ AM_LDFLAGS +=                                   \
   $(BOOST_SERIALIZATION_LDFLAGS)                \
   $(BOOST_UNIT_TEST_FRAMEWORK_LDFLAGS)
 LDADD +=                                        \
-  $(LIBPORT_LIBS)				\
+  $(LIBPORT_LIBS)                               \
   $(BOOST_SERIALIZATION_LIBS)                   \
   $(BOOST_UNIT_TEST_FRAMEWORK_LIBS)
 
 
-tests_libport_asio_SOURCES			  = tests/libport/asio.cc
+tests_libport_asio_SOURCES                        = tests/libport/asio.cc
 tests_libport_asio_LDADD = $(BOOST_SYSTEM_LIBS)  $(LDADD)
 tests_libport_asio_LDFLAGS = $(BOOST_SYSTEM_LDFLAGS) $(PTHREAD_LDFLAGS) $(AM_LDFLAGS)
 tests_libport_asio_CXXFLAGS = $(PTHREAD_CFLAGS) $(AM_CXXFLAGS)
 
-tests_libport_base64_SOURCES                    = tests/libport/base64.cc
-tests_libport_backtrace_SOURCES			= tests/libport/backtrace.cc
-tests_libport_cli_SOURCES                       = tests/libport/cli.cc
-tests_libport_condition_SOURCES                 = tests/libport/condition.cc
-tests_libport_cstdlib_SOURCES                   = tests/libport/cstdlib.cc
-tests_libport_debug_SOURCES                     = tests/libport/debug.cc
-tests_libport_deref_SOURCES                     = tests/libport/deref.cc
-tests_libport_dirent_SOURCES                    = tests/libport/dirent.cc
-tests_libport_erase_if_SOURCES                  = tests/libport/erase-if.cc
-tests_libport_escape_SOURCES                    = tests/libport/escape.cc
-tests_libport_fd_stream_SOURCES                 = tests/libport/fd-stream.cc
-tests_libport_fifo_SOURCES                      = tests/libport/fifo.cc
-tests_libport_file_library_SOURCES              = tests/libport/file-library.cc
-tests_libport_finally_SOURCES                   = tests/libport/finally.cc
-tests_libport_foreach_SOURCES                   = tests/libport/foreach.cc
-tests_libport_has_if_SOURCES                    = tests/libport/has-if.cc
-tests_libport_indent_SOURCES                    = tests/libport/indent.cc
-tests_libport_intrusive_ptr_SOURCES             = tests/libport/intrusive-ptr.cc
-tests_libport_intrusive_ptr_serialize_SOURCES   = tests/libport/intrusive-ptr-serialize.cc
-tests_libport_io_stream_SOURCES                 = tests/libport/io-stream.cc
-tests_libport_markup_ostream_SOURCES		= tests/libport/markup-ostream.cc
-tests_libport_path_SOURCES                      = tests/libport/path.cc
-tests_libport_pid_file_SOURCES                  = tests/libport/pid-file.cc
-tests_libport_pthread_SOURCES                   = tests/libport/pthread.cc
-tests_libport_semaphore_SOURCES                 = tests/libport/semaphore.cc
-tests_libport_separator_SOURCES                 = tests/libport/separator.cc
-tests_libport_singleton_ptr_SOURCES             = tests/libport/singleton-ptr.cc
-tests_libport_statistics_SOURCES                = tests/libport/statistics.cc
-tests_libport_symbol_SOURCES                    = tests/libport/symbol.cc
-tests_libport_time_SOURCES                      = tests/libport/time.cc
-tests_libport_timer_SOURCES                     = tests/libport/timer.cc
-tests_libport_tokenizer_SOURCES                 = tests/libport/tokenizer.cc
-tests_libport_ufloat_double_SOURCES             = tests/libport/ufloat-double.cc
-tests_libport_unescape_SOURCES                  = tests/libport/unescape.cc
-tests_libport_utime_SOURCES                     = tests/libport/utime.cc
+tests_libport_base64_SOURCES                            = tests/libport/base64.cc
+tests_libport_backtrace_SOURCES                         = tests/libport/backtrace.cc
+tests_libport_cli_SOURCES                               = tests/libport/cli.cc
+tests_libport_condition_SOURCES                         = tests/libport/condition.cc
+tests_libport_cstdlib_SOURCES                           = tests/libport/cstdlib.cc
+tests_libport_damerau_levenshtein_distance_SOURCES      = tests/libport/damerau-levenshtein-distance.cc
+tests_libport_debug_SOURCES                             = tests/libport/debug.cc
+tests_libport_deref_SOURCES                             = tests/libport/deref.cc
+tests_libport_dirent_SOURCES                            = tests/libport/dirent.cc
+tests_libport_erase_if_SOURCES                          = tests/libport/erase-if.cc
+tests_libport_escape_SOURCES                            = tests/libport/escape.cc
+tests_libport_fd_stream_SOURCES                         = tests/libport/fd-stream.cc
+tests_libport_fifo_SOURCES                              = tests/libport/fifo.cc
+tests_libport_file_library_SOURCES                      = tests/libport/file-library.cc
+tests_libport_finally_SOURCES                           = tests/libport/finally.cc
+tests_libport_foreach_SOURCES                           = tests/libport/foreach.cc
+tests_libport_has_if_SOURCES                            = tests/libport/has-if.cc
+tests_libport_indent_SOURCES                            = tests/libport/indent.cc
+tests_libport_intrusive_ptr_SOURCES                     = tests/libport/intrusive-ptr.cc
+tests_libport_intrusive_ptr_serialize_SOURCES           = tests/libport/intrusive-ptr-serialize.cc
+tests_libport_io_stream_SOURCES                         = tests/libport/io-stream.cc
+tests_libport_markup_ostream_SOURCES                    = tests/libport/markup-ostream.cc
+tests_libport_path_SOURCES                              = tests/libport/path.cc
+tests_libport_pid_file_SOURCES                          = tests/libport/pid-file.cc
+tests_libport_pthread_SOURCES                           = tests/libport/pthread.cc
+tests_libport_semaphore_SOURCES                         = tests/libport/semaphore.cc
+tests_libport_separator_SOURCES                         = tests/libport/separator.cc
+tests_libport_singleton_ptr_SOURCES                     = tests/libport/singleton-ptr.cc
+tests_libport_statistics_SOURCES                        = tests/libport/statistics.cc
+tests_libport_symbol_SOURCES                            = tests/libport/symbol.cc
+tests_libport_time_SOURCES                              = tests/libport/time.cc
+tests_libport_timer_SOURCES                             = tests/libport/timer.cc
+tests_libport_tokenizer_SOURCES                         = tests/libport/tokenizer.cc
+tests_libport_ufloat_double_SOURCES                     = tests/libport/ufloat-double.cc
+tests_libport_unescape_SOURCES                          = tests/libport/unescape.cc
+tests_libport_utime_SOURCES                             = tests/libport/utime.cc
 
-EXTRA_DIST +=					\
-  tests/libport/ufloat-common.cc		\
-  tests/libport/ufloat-config.h			\
+EXTRA_DIST +=                                   \
+  tests/libport/ufloat-common.cc                \
+  tests/libport/ufloat-config.h                 \
   tests/libport/666.txt
 
-TESTS_ENVIRONMENT +=				\
+TESTS_ENVIRONMENT +=                            \
   SRCDIR=$(srcdir)
 
 CLEANFILES += tests/libport/exists.pid
