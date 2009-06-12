73919997c4ce062d75fee03ddc7e3ff1bfd4f773
mefyl <hocquet@gostai.com>
2009-06-12 19:03:02 +0200

    Don't link serialize against tiny-xml.
    	* lib/serialize/local.mk: Here.

--- lib/serialize/local.mk
+++ lib/serialize/local.mk
@@ -1,6 +1,6 @@
 lib_LTLIBRARIES += lib/serialize/libserialize.la
 lib_serialize_libserialize_la_LDFLAGS = -avoid-version -no-undefined
-lib_serialize_libserialize_la_LIBADD = $(LIBADD) $(TINYXML_LIBS)
+lib_serialize_libserialize_la_LIBADD = $(LIBADD)
 lib_serialize_libserialize_la_CPPFLAGS =	\
   $(AM_CPPFLAGS)				\
   -DBUILDING_SERIALIZE
