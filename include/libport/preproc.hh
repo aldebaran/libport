fc003888e0b8b0462b81a1cc0366a489f8c7d582
mefyl <hocquet@gostai.com>
2009-06-16 13:34:41 +0200

    Add libport::preproc: preprocessor utilities above boost::pp.
    	* include/libport/preproc.hh: New.

---
+++ include/libport/preproc.hh
@@ -0,0 +1,37 @@
+#ifndef LIBPORT_PREPROC_HH
+# define LIBPORT_PREPROC_HH
+
+# include <boost/function.hpp>
+# include <boost/preprocessor/tuple/elem.hpp>
+# include <boost/preprocessor/seq/for_each.hpp>
+
+# define LIBPORT_MAP_HELPER(R, Macro, Elt) (Macro(Elt))
+/// Return \a Seq, with \a Macro applied on every element.
+# define LIBPORT_MAP(Macro, Seq)                                 \
+  BOOST_PP_SEQ_FOR_EACH(LIBPORT_MAP_HELPER, Macro, Seq)          \
+
+# define LIBPORT_APPLY_HELPER(R, Macro, Elt) Macro(Elt)
+/// Apply \a Macro applied on every element of \a Seq.
+# define LIBPORT_APPLY(Macro, Seq)                               \
+  BOOST_PP_SEQ_FOR_EACH(LIBPORT_APPLY_HELPER, Macro, Seq)        \
+
+/// Return the first element of \a Pair
+# define LIBPORT_FIRST(Pair) BOOST_PP_TUPLE_ELEM(2, 0, Pair)
+/// Return the second element of \a Pair
+# define LIBPORT_SECOND(Pair) BOOST_PP_TUPLE_ELEM(2, 1, Pair)
+
+/// Return the first element of \a Seq
+# define LIBPORT_HEAD(Seq) BOOST_PP_SEQ_HEAD(Seq)
+/// Return \a Seq, minus the first element
+# define LIBPORT_TAIL(Seq) BOOST_PP_SEQ_TAIL(Seq)
+
+# define LIBPORT_SEPARATE_HELPER(Elt) , Elt
+/// Separate \a Seq with commas
+# define LIBPORT_SEPARATE(Seq)                                          \
+  LIBPORT_HEAD(Seq) LIBPORT_APPLY(LIBPORT_SEPARATE_HELPER, LIBPORT_TAIL(Seq))
+
+/// Concatenate the two arguments
+# define LIBPORT_CAT BOOST_PP_CAT
+
+
+#endif
