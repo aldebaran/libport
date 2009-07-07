8258d6f7891ce697ca04001c35eaf34622e32874
Akim Demaille <demaille@gostai.com>
2009-07-07 16:19:54 +0200

    valgrind.hh: fix.
    	* include/libport/valgrind.hh: Several fixes.

--- include/libport/valgrind.hh
+++ include/libport/valgrind.hh
@@ -1,8 +1,10 @@
 #ifndef LIBPORT_VALGRIND_HH
 # define LIBPORT_VALGRIND_HH
 
-# ifdef HAVE_VALGRIND_VALGRIND_HH
-#  include <valgrind/valgrind.hh>
+# include <libport/config.h>
+
+# ifdef LIBPORT_HAVE_VALGRIND_VALGRIND_H
+#  include <valgrind/valgrind.h>
 # else
 #  define RUNNING_ON_VALGRIND 0
 # endif
