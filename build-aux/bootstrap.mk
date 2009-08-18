58c63235b2cb29f37ab37f8cbd3dfacd91808498
Akim Demaille <demaille@gostai.com>
2009-08-18 11:15:49 +0200

    build: fix bootstrap.
    	* build-aux/bootstrap.mk: Fix confusion between bootstrap.stamp
    	and boostrap.mk.

--- build-aux/bootstrap.mk
+++ build-aux/bootstrap.mk
@@ -8,11 +8,12 @@ MAINTAINERCLEANFILES += $(srcdir)/bootstrap.stamp
 
 BOOTSTRAP_sources =				\
   $(BOOTSTRAP_CFG)				\
-  $(build_aux_dir)/bootstrap			\
+  $(build_aux_dir)/bootstrap
+
+dist_noinst_DATA +=				\
+  $(BOOTSTRAP_sources)				\
   $(srcdir)/bootstrap.stamp
 
-dist_noinst_DATA += $(BOOTSTRAP_sources)
-
-$(srcdir)/bootstrap.stamp: $(BOOTSTRAP_sources)
+$(srcdir)/bootstrap.stamp: $(BOOTSTRAP_sources) $(build_aux_dir)/bootstrap.mk
 	@echo "Bootstrapping again because of $?"
 	cd $(top_srcdir) && ./bootstrap
