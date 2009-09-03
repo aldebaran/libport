be1bda3646540d0d3accfb4f84c32249001ab80b
Akim Demaille <demaille@gostai.com>
2009-09-03 16:17:59 +0200

    Copyright/license changes.

--- include/libport/format.hh.py
+++ include/libport/format.hh.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+# Copyright (C) 2009, Gostai S.A.S.
+#
+# This software is provided "as is" without warranty of any kind,
+# either expressed or implied, including but not limited to the
+# implied warranties of fitness for a particular purpose.
+#
+# See the LICENSE file for more information.
+
+
 print '''\
 #ifndef LIBPORT_FORMAT_HH
 # define LIBPORT_FORMAT_HH
