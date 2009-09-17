f6296543747c01342190c941277592b285a9cfb8
Akim Demaille <demaille@gostai.com>
2009-09-17 15:54:29 +0200

    Fix.

--- include/libport/windows.hh
+++ include/libport/windows.hh
@@ -34,11 +34,11 @@
 #   define NOMINMAX 1
 #  endif
 
-#  include <system-warning-push.hh>
+#  include <libport/system-warning-push.hh>
 #   include <io.h>
 #   include <process.h>
 #   include <windows.h>
-#  include <system-warning-pop.hh>
+#  include <libport/system-warning-pop.hh>
 
 /* If we defined _WINSOCKAPI_ to prevent WinSock1 stuff to be imported,
  * restore the situation since the user might really want to import WinSock.h
