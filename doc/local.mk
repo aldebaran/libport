77f1d3eef562991c0c712c9aa9d28a23d802ca1c
Akim Demaille <demaille@gostai.com>
2009-11-03 12:07:07 +0100

    doc/libport.tex.
    	* .gitmodules: New.
    	* doc/document-aux: New.
    	* doc/libport.tex: New.
    	* doc/local.mk: Adjust to use it.

--- doc/local.mk
+++ doc/local.mk
@@ -25,3 +25,41 @@ clean-local:
 
 EXTRA_DIST += doc/doxygen.sh.in
 CLEANFILES += doc/Doxyfile doc/doxygen.sh
+
+
+## ------- ##
+## LaTeX.  ##
+## ------- ##
+
+# Document aux.
+share_dir = $(srcdir)/doc/document-aux
+share_bin_dir = $(share_dir)/bin
+share_make_dir = $(share_dir)/make
+include doc/document-aux/make/tex.mk
+EXTRA_DIST +=								   \
+  $(addprefix doc/document-aux/,$(call ls_files_in_dir,$(share_dir)))
+
+TEXI2DVI_FLAGS += -I doc -~
+
+PDF = doc/libport.pdf
+EXTRA_DATA += $(PDF)
+CLEANFILES += $(PDF)
+
+REVISION_FILE_STY = doc/revision.sty
+$(REVISION_FILE_STY): $(top_srcdir)/.version $(REVISION) $(build_aux_dir)/revision.mk
+	$(REVISION) --latex			\
+		--cache=$<			\
+		--srcdir=$(top_srcdir)		\
+		--directory			\
+		--output=$@
+	touch $@
+
+libport_sources = $(call ls_files,doc/*.tex)
+EXTRA_DIST += $(libport_sources)
+libport_deps =					\
+  $(REVISION_FILE_STY)				\
+  $(libport_sources)
+#   $(call ls_files,doc/*.sty doc/*.cls)
+$(PDF): $(libport_deps)
+
+
