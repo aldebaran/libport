b5ef484b188e8ba96b7314c4996b9f9ec4128511
Akim Demaille <demaille@gostai.com>
2010-01-05 15:11:38 +0100

    msvc: be robust to double definition of WIN32.
    Now that URBI_PROG_CXX define WIN32 to 1 (instead of #define WIN32),
    we have complaints about multiple definitions.  So use the same value.
    
    	* include/libport/detect-win32.h (WIN32): Define to 1.

--- include/libport/detect-win32.h
+++ include/libport/detect-win32.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2010, Gostai S.A.S.
+ *
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
+ *
+ * See the LICENSE file for more information.
+ */
+
 #ifndef LIBPORT_DETECT_WIN32_H
 # define LIBPORT_DETECT_WIN32_H
 
@@ -7,11 +17,19 @@
 #  endif
 # endif
 
+
 # ifdef WIN32
+
+// Normalize to 1
+#  undef WIN32
+#  define WIN32
+
 #  ifndef _WIN32_WINNT
 #   define _WIN32_WINNT 0x0400
 #  endif
+
 # endif /* !WIN32 */
+
 
 # ifdef WIN32
 #  define WIN32_IF(Then, Else) Then
