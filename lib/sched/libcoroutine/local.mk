009f8d47f8686751e26d4d6ef277e19ad68960f0
Akim Demaille <demaille@gostai.com>
2010-01-14 15:31:23 +0100

    sched: fix build.
    	* lib/sched/libcoroutine/local.mk, lib/sched/local.mk: Fix the name
    	of the libraries.

--- lib/sched/libcoroutine/local.mk
+++ lib/sched/libcoroutine/local.mk
@@ -1,4 +1,4 @@
-dist_lib_sched_libsched_la_SOURCES +=		\
+dist_lib_sched_libsched@LIBSFX@_la_SOURCES +=	\
   lib/sched/libcoroutine/Base.h			\
   lib/sched/libcoroutine/Coro.cc		\
   lib/sched/libcoroutine/context.cc		\
--- lib/sched/local.mk
+++ lib/sched/local.mk
@@ -1,19 +1,19 @@
 lib_LTLIBRARIES += lib/sched/libsched@LIBSFX@.la
 lib_sched_libsched@LIBSFX@_la_LDFLAGS = -avoid-version -no-undefined
 lib_sched_libsched@LIBSFX@_la_LIBADD = $(LIBADD) $(LIBPORT_LIBS)
-lib_sched_libsched@LIBSFX@_la_CPPFLAGS =		\
+lib_sched_libsched@LIBSFX@_la_CPPFLAGS =	\
   $(AM_CPPFLAGS)				\
   -DBUILDING_LIBSCHED
 
 if PTHREAD_SOURCES
-lib_sched_libsched@LIBSFX@_la_CPPFLAGS +=               \
-  -I$(PTHREAD_SOURCES)                          \
-  -I$(PTHREAD_SOURCES)/linuxthreads             \
-  -I$(PTHREAD_SOURCES)/linuxthreads/sysdeps/pthread  \
+lib_sched_libsched@LIBSFX@_la_CPPFLAGS +=		\
+  -I$(PTHREAD_SOURCES)					\
+  -I$(PTHREAD_SOURCES)/linuxthreads			\
+  -I$(PTHREAD_SOURCES)/linuxthreads/sysdeps/pthread	\
   -I$(PTHREAD_SOURCES)/linuxthreads/sysdeps/arm
 endif
 
-dist_lib_sched_libsched@LIBSFX@_la_SOURCES =		\
+dist_lib_sched_libsched@LIBSFX@_la_SOURCES =	\
   lib/sched/configuration.cc			\
   lib/sched/job.cc				\
   lib/sched/pthread-coro.cc			\
@@ -23,7 +23,7 @@ dist_lib_sched_libsched@LIBSFX@_la_SOURCES =		\
   lib/sched/tag.cc				\
   lib/sched/uclibc-workaround.cc
 
-lib_sched_libsched_la_CPPFLAGS +=               \
+lib_sched_libsched@LIBSFX@_la_CPPFLAGS +=	\
   -I$(top_srcdir)/include/sched/libcoroutine
 
 # This is distributed, but not compiled directly: it is included by
