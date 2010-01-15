4391109deb7c1fbb26a84f3b83b9cc7d6b71a95a
Akim Demaille <demaille@gostai.com>
2010-01-15 11:31:29 +0100

    Formatting changes.

--- tests/sched/local.mk
+++ tests/sched/local.mk
@@ -2,8 +2,8 @@ if ! SCHED_CORO_OSTHREAD
 ## This test is directly checking the coroutine interface, not
 ## the sched interface.
 TESTS_BINARIES +=				\
-		tests/sched/sched.cc	        \
-		tests/sched/sched-except.cc
+  tests/sched/sched.cc				\
+  tests/sched/sched-except.cc
 endif
 
 tests_sched_sched_SOURCES = tests/sched/sched.cc
