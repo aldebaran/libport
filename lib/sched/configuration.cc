32c939c325111756191e6f0c498a151cf174ae54
Akim Demaille <demaille@gostai.com>
2010-01-21 11:15:05 +0100

    sched: larger stack size.
    2.x/uvar/onchange-exception.chk fails in debug mode because of stack
    exhaustion.
    
    	* build-aux: Update: no longer define URBI_KERNEL_STACK_SIZE.
    	* lib/sched/configuration.cc: Compute it here.

--- lib/sched/configuration.cc
+++ lib/sched/configuration.cc
@@ -12,11 +12,22 @@
 
 namespace sched
 {
+
+#if defined LIBPORT_COMPILATION_MODE_SPACE
+# define STACK_SIZE 128
+#else
+# define STACK_SIZE 512
+#endif
+
+#if defined LIBPORT_COMPILATION_MODE_DEBUG
+# define STACK_SIZE (STACK_SIZE * 4)
+#endif
+
   Configuration configuration =
   {
     // Default is large enough until we enable a mechanism to dynamically
     // reallocate task space on demand.
-    /* .default_stack_size = */   LIBPORT_URBI_KERNEL_STACK_SIZE * 1024,
-    /* .minimum_stack_size = */   LIBPORT_URBI_KERNEL_STACK_SIZE * 1024 / 8,
+    /* .default_stack_size = */   STACK_SIZE * 1024,
+    /* .minimum_stack_size = */   STACK_SIZE * 1024 / 8,
   };
 }
