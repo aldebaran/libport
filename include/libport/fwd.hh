9057bc56ad8f3f1f9f4686b15e01aa2230c969bd
Akim Demaille <demaille@gostai.com>
2010-01-22 11:35:54 +0100

    libport: dead code.
    	* include/libport/fwd.hh: Remove obsolete dead code.

--- include/libport/fwd.hh
+++ include/libport/fwd.hh
@@ -12,63 +12,6 @@
 
 namespace libport
 {
-#if 0
-  // We don't fwddecl hash, because it is too much of a PITA:
-
-  // namespace ns
-  // {
-  //   struct foo;
-  // }
-  //
-  // namespace impl
-  // {
-  //   struct foo {};
-  // }
-  //
-  // namespace ns
-  // {
-  //   using impl::foo;
-  // }
-
-  // does not work, the compiler complains there are two definitions
-  // of ns::foo.  The following works:
-
-  // namespace impl
-  // {
-  //   template <typename T>
-  //   struct foo;
-  // }
-  //
-  // namespace ns
-  // {
-  //   using impl::foo;
-  // }
-  //
-  // namespace impl
-  // {
-  //   template <typename T>
-  //   struct foo {};
-  // }
-  //
-  // namespace ns
-  // {
-  //   using impl::foo;
-  // }
-
-  // but the problem is that this definition will depend upon the
-  // architecture to find what the "impl" namespace is.  So we
-  // just don't fwddecl hash, unless someone feels like filling the
-  // blanks.
-
-  // hash.hh.
-  // Warning: The VC++ implementation might have a different number of
-  // parameters, in which case a #if will be needed to fwd decl both
-  // flavors of hash_map.
-  // Anyway, we want std::tr1::unodered_map here.
-  template<class _Key, class _Tp, class _HashFcn, class _EqualKey, class _Alloc>
-  class hash_map;
-#endif
-
   // asio.hh;
   struct ConcreteSocket;
   struct Socket;
