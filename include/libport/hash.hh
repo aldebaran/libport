c638b54417cacfef4c7133d5f117de99725bd0a3
Akim Demaille <demaille@gostai.com>
2010-02-04 11:42:42 +0100

    Libport.Hash: disable now useless pragma.
    	* include/libport/hash.hh: here.

--- include/libport/hash.hh
+++ include/libport/hash.hh
@@ -11,11 +11,30 @@
 #ifndef LIBPORT_HASH_HH
 # define LIBPORT_HASH_HH
 
-# include <boost/version.hpp>
-# include <libport/config.h>
+// The following is useless.
+//
+// It was an attempt to fix a warning in Boost.Unordered that is not
+// fixed yet, but appeared when we moved to the older 1.38.  Actually
+// the problem was that we picked up the Boost in /opt/local/include
+// because X11_CPPFLAGS (needed for programs such as
+// urbi-balltracking) included "-I /opt/local/include".  And since we
+// had "-isystem .../boost-1.38" and system dirs were treated last,
+// (1) we were including the wrong copy of boost, and (2) it was
+// included via -I instead of -isystem, whcih resulted in many useless
+// warnings.
+//
+// The following pragma was adressing one such warning.
+//
+// But since then we also use -isystem for X11_CPPFLAGS, so (1) we use
+// the right Boost, (2) whose warnings are covered by -isystem.
 
-# if BOOST_VERSION <= 103800 && LIBPORT_HAVE_PRAGMA_GCC_DIAGNOSTIC
-#  pragma GCC diagnostic warning "-Wunused-parameter"
+#if 0
+#  include <boost/version.hpp>
+#  include <libport/config.h>
+
+#  if BOOST_VERSION <= 104100 && LIBPORT_HAVE_PRAGMA_GCC_DIAGNOSTIC
+#   pragma GCC diagnostic warning "-Wunused-parameter"
+#  endif
 # endif
 
 # include <boost/unordered_map.hpp>
