265d2b7af33c61b2703e1bdabc00902023e931e1
Quentin Hocquet <hocquet@gostai.com>
2010-03-16 17:30:30 +0100

    Greatly improve libport/attributes.
    	* include/libport/attributes.hh: Here. See inline documentation.

--- include/libport/attributes.hh
+++ include/libport/attributes.hh
@@ -11,33 +11,102 @@
 #ifndef LIBPORT_ATTRIBUTES_HH
 # define LIBPORT_ATTRIBUTES_HH
 
+# include <libport/preproc.hh>
 # include <libport/traits.hh>
 
-#define ATTRIBUTE(Type, Name)                   \
+/* ATTRIBUTE_{mode}(Type, Name, [Getter modifiers], [Setter modifiers], [Member modifier])
+ *
+ * mode:
+ * - nothing: generate only the membur.
+ * - R: generate a getter.
+ * - W: generate a setter.
+ * - RW: generate both.
+ * R and W can be used lowercase, in which case only the prototype of
+ * the getter/setter is added. You can then provide your own
+ * implementation.
+ *
+ * Type: type of the attribute.
+ * Name: name of the attribute.
+ * Getter/Setter modifiers: modifiers for the getter/setter (virtual, static, ...).
+ * Member modifier: modifier for the member (mutable, static, ...).
+ */
+
+
+#define LIBPORT_VARIADIC(...) LIBPORT_LIST_TAIL(LIBPORT_LIST(__VA_ARGS__,))
+
+#define ATTRIBUTE(Type, ...)                  \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), NONE , NONE , LIBPORT_VARIADIC(__VA_ARGS__)); \
+
+#define ATTRIBUTE_R(Type, ...)                  \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), IMPL , NONE , LIBPORT_VARIADIC(__VA_ARGS__)); \
+
+#define ATTRIBUTE_r(Type, ...)                  \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), PROTO, NONE , LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_W(Type, ...)                  \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), NONE , IMPL , LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_w(Type, ...)                  \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), NONE , PROTO, LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_RW(Type, ...)                 \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), IMPL , IMPL , LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_rw(Type, ...)                 \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), PROTO, PROTO, LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_Rw(Type, ...)                 \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), IMPL , PROTO, LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+#define ATTRIBUTE_rW(Type, ...)                 \
+  ATTRIBUTE_BOUNCE(Type, LIBPORT_LIST_HEAD(LIBPORT_LIST(__VA_ARGS__,)), PROTO, IMPL , LIBPORT_VARIADIC(__VA_ARGS__));   \
+
+
+/*----------.
+| Helpers.  |
+`----------*/
+
+#define ATTRIBUTE_MEMBER(Type, Name, Mods)      \
   private:                                      \
-  Type Name ## _;
+  Mods Type Name ## _;                          \
 
-#define ATTRIBUTE_GETTER(Type, Name)                    \
-  public:                                               \
-  libport::traits::Arg<Type>::res Name ## _get() const  \
-  { return Name ## _; }                                 \
+#define ATTRIBUTE_BOUNCE(Type, Name, Get, Set, Modifiers)               \
+  ATTRIBUTE_GETTER_##Get(Type, Name, LIBPORT_LIST_NTH(0, Modifiers))    \
+  ATTRIBUTE_SETTER_##Set(Type, Name, LIBPORT_LIST_NTH(1, Modifiers))    \
+  ATTRIBUTE_MEMBER(Type, Name, LIBPORT_LIST_NTH(2, Modifiers))          \
 
-#define ATTRIBUTE_SETTER(Type, Name)                            \
+/*---------.
+| Getter.  |
+`---------*/
+
+#define ATTRIBUTE_GETTER(Type, Name, Mods)                       \
+  public:                                                        \
+  Mods libport::traits::Arg<Type>::res Name ## _get() const      \
+
+#define ATTRIBUTE_GETTER_NONE(Type, Name, Mods)
+
+#define ATTRIBUTE_GETTER_PROTO(Type, Name, Mods)        \
+  ATTRIBUTE_GETTER(Type, Name, Mods);                   \
+
+#define ATTRIBUTE_GETTER_IMPL(Type, Name, Mods) \
+  ATTRIBUTE_GETTER(Type, Name, Mods)            \
+  { return Name ## _; }                         \
+
+/*---------.
+| Setter.  |
+`---------*/
+
+#define ATTRIBUTE_SETTER(Type, Name, Mods)                      \
   public:                                                       \
-  void Name ## _set(libport::traits::Arg<Type>::res val)        \
-  { Name ## _ = val; }                                          \
+  Mods void Name ## _set(libport::traits::Arg<Type>::res val)   \
 
-#define ATTRIBUTE_R(Type, Name)                 \
-  ATTRIBUTE(Type, Name);                        \
-  ATTRIBUTE_GETTER(Type, Name);
+#define ATTRIBUTE_SETTER_NONE(Type, Name, Mods)
 
-#define ATTRIBUTE_W(Type, Name)                 \
-  ATTRIBUTE(Type, Name);                        \
-  ATTRIBUTE_SETTER(Type, Name);
+#define ATTRIBUTE_SETTER_PROTO(Type, Name, Mods)        \
+  ATTRIBUTE_SETTER(Type, Name, Mods);                   \
 
-#define ATTRIBUTE_RW(Type, Name)                \
-  ATTRIBUTE(Type, Name);                        \
-  ATTRIBUTE_GETTER(Type, Name);                 \
-  ATTRIBUTE_SETTER(Type, Name);
+#define ATTRIBUTE_SETTER_IMPL(Type, Name, Mods) \
+  ATTRIBUTE_SETTER(Type, Name, Mods)            \
+  { Name ## _ = val; }                          \
 
 #endif
