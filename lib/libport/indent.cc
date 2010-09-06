134f83b4e818de2ec46a9e249fe35e2c18d4fb35
Akim Demaille <demaille@gostai.com>
2010-09-06 15:22:10 +0200

    libport.indent: use libport.xalloc.
    	* lib/libport/indent.cc: here.

--- lib/libport/indent.cc
+++ lib/libport/indent.cc
@@ -18,37 +18,33 @@
 #include <libport/cassert>
 
 #include <libport/indent.hh>
+#include <libport/xalloc.hh>
 
 namespace libport
 {
 
   namespace
   {
-    /// The current indentation level for \a o.
-    inline long int& indent_get(std::ostream& o)
-    {
-      // The slot to store the current indentation level.
-      static const long int indent_index = std::ios::xalloc();
-      return o.iword(indent_index);
-    }
+    typedef xalloc<long int>indentation_type;
+    indentation_type indentation;
   }
 
   std::ostream& incindent(std::ostream& o)
   {
-    indent_get(o) += 2;
+    indentation(o) += 2;
     return o;
   }
 
   std::ostream& decindent(std::ostream& o)
   {
-    aver(indent_get(o));
-    indent_get(o) -= 2;
+    aver(indentation(o));
+    indentation(o) -= 2;
     return o;
   }
 
   std::ostream& resetindent(std::ostream& o)
   {
-    indent_get(o) = 0;
+    indentation(o) = 0;
     return o;
   }
 
@@ -56,7 +52,7 @@ namespace libport
   {
     // Be sure to be able to restore the stream flags.
     char fill = o.fill(' ');
-    return o << std::setw(indent_get(o))
+    return o << std::setw(indentation(o))
 	     << ""
 	     << std::setfill(fill);
   }
