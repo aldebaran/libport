e533aedd9a915447a3993adf91e06969c4c874ea
Akim Demaille <demaille@gostai.com>
2010-12-06 15:31:38 +0100

    build: ship files.
    	* include/sched/local.mk: here.

--- include/sched/local.mk
+++ include/sched/local.mk
@@ -8,23 +8,25 @@
 
 # Currently we don't want to install libsched headers.
 sched_includedir =
-sched_include_HEADERS =					\
-  include/sched/configuration.hh			\
-  include/sched/coroutine.hh				\
-  include/sched/coroutine.hxx				\
-  include/sched/coroutine-coro.hxx			\
-  include/sched/coroutine-data.hh			\
-  include/sched/coroutine-data.hxx			\
-  include/sched/coroutine-hooks.hh			\
-  include/sched/exception.hh				\
-  include/sched/exception.hxx				\
-  include/sched/export.hh				\
-  include/sched/fwd.hh					\
-  include/sched/job.hh					\
-  include/sched/job.hxx					\
-  include/sched/scheduler.hh				\
-  include/sched/scheduler.hxx				\
-  include/sched/tag.hh					\
+sched_include_HEADERS =				\
+  include/sched/configuration.hh		\
+  include/sched/coroutine.hh			\
+  include/sched/coroutine.hxx			\
+  include/sched/coroutine-coro.hxx		\
+  include/sched/coroutine-data.hh		\
+  include/sched/coroutine-data.hxx		\
+  include/sched/coroutine-hooks.hh		\
+  include/sched/coroutine-local-storage.hh	\
+  include/sched/coroutine-local-storage.hxx	\
+  include/sched/exception.hh			\
+  include/sched/exception.hxx			\
+  include/sched/export.hh			\
+  include/sched/fwd.hh				\
+  include/sched/job.hh				\
+  include/sched/job.hxx				\
+  include/sched/scheduler.hh			\
+  include/sched/scheduler.hxx			\
+  include/sched/tag.hh				\
   include/sched/tag.hxx
 
 include include/sched/libcoroutine/local.mk
