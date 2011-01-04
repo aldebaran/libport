3262de0ff4dbf77c85053f7511bb772147ef6353
Quentin Hocquet <hocquet@gostai.com>
2011-01-04 15:10:58 +0100

    Debug: fix test: test the indentation level through coroutines.
    	* tests/sched/debug.cc: Here, instead of testing the dump level,
    	which isn't coroutine-local anymore.

--- tests/sched/debug.cc
+++ tests/sched/debug.cc
@@ -44,108 +44,94 @@ GD_CATEGORY(MAIN);
 
 #ifndef LIBPORT_DEBUG_DISABLE
 
-#define ECHO(S)                                 \
-  std::cerr << S << std::endl
-
 Coro* mc;
-Coro* c1, *c2;
+Coro* c1;
+Coro* c2;
 
-static void start2(void*)
-{
-  GD_CATEGORY(C2);
-  GD_FILTER_LOG();
-  ECHO("(04) c2 start");
-
-  GD_INFO_LOG("= C2");
-
-  BOOST_CHECK_EQUAL(GD_CURRENT_LEVEL(), libport::Debug::levels::log);
-  GD_FILTER_DEC();
-
-  ECHO("(05) c2->c1");
-  coroutine_switch_to(c2, c1);
-
-  GD_INFO_LOG("= C2 (should not be printed)");
-
-  // should not be libport::Debug::levels::dump
-  BOOST_CHECK_EQUAL(GD_CURRENT_LEVEL(), libport::Debug::levels::none);
-  BOOST_CHECK(!GD_SHOW_TRACE());
-  GD_FILTER_INC();
-
-  GD_INFO_LOG("= C2");
-
-  ECHO("(10) c2->main");
-  coroutine_switch_to(c2, mc);
-
-  ECHO("(!!) END!!");
-}
-
-static void start(void*)
-{
-  GD_CATEGORY(C1);
-  GD_FILTER_DUMP();
-  ECHO("(02) c1 start");
-  c2 = coroutine_new();
-
-  GD_INFO_LOG("= C1");
-
-  BOOST_CHECK_EQUAL(GD_CURRENT_LEVEL(), libport::Debug::levels::dump);
-  GD_FILTER_INC();
-
-  ECHO("(03) c1->start c2");
-  coroutine_start(c1, c2, &start2, (void*)0);
-
-  GD_INFO_LOG("= C1");
-
-  // should not be libport::Debug::levels::none
-  BOOST_CHECK_EQUAL(GD_CURRENT_LEVEL(), libport::Debug::levels::dump);
-  BOOST_CHECK(GD_SHOW_DUMP());
-
-  ECHO("(06) c1->main");
-  coroutine_switch_to(c1, mc);
-
-  GD_INFO_LOG("= C1");
-
-  ECHO("(08) c1->main");
-  coroutine_switch_to(c1, mc);
-
-  GD_INFO_LOG("= C1");
-
-  ECHO("(!!) END!!");
-}
+static void start_c1(void*);
+static void start_c2(void*);
 
 // check that debug is coroutine safe.
 void test_debug()
 {
   GD_CATEGORY(MAIN);
-
-  // test the category before the main coroutine defined.
-  GD_INFO_LOG("= Main");
-
-  ECHO("(00) Main start");
+  GD_INFO_LOG("Main start");
 
   mc = coroutine_new();
   coroutine_initialize_main(mc);
   c1 = coroutine_new();
 
-  GD_INFO_LOG("= Main");
+  {
+    GD_PUSH("Main indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+    GD_INFO_LOG("Main => C1");
+    coroutine_start(mc, c1, &start_c1, (void*)0);
+    GD_INFO_LOG("Main <= C1");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+    GD_INFO_LOG("Main => C1");
+    coroutine_switch_to(mc, c1);
+    GD_INFO_LOG("Main <= C1");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+    GD_INFO_LOG("Main => C2");
+    coroutine_switch_to(mc, c2);
+    GD_INFO_LOG("Main <= C2");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+  }
+  BOOST_CHECK_EQUAL(GD_INDENTATION(), 0);
 
-  ECHO("(01) Main->start c1");
-  coroutine_start(mc, c1, &start, (void*)0);
-
-  GD_INFO_LOG("= Main");
-
-  ECHO("(07) Main->c1");
-  coroutine_switch_to(mc, c1);
-
-  GD_INFO_LOG("= Main");
-
-  ECHO("(09) Main->c2");
-  coroutine_switch_to(mc, c2);
-
-  GD_INFO_LOG("= Main");
-
-  ECHO("(11) Main done");
+  GD_INFO_LOG("Main end");
   BOOST_CHECK_NO_THROW(GD_QUIT());
+}
+
+static void start_c1(void*)
+{
+  GD_CATEGORY(C1);
+  GD_INFO_LOG("C1 start");
+  c2 = coroutine_new();
+  BOOST_CHECK_EQUAL(GD_INDENTATION(), 0);
+  GD_INFO_LOG("C1 => C2");
+  coroutine_start(c1, c2, &start_c2, (void*)0);
+  GD_INFO_LOG("C1 <= C2");
+  BOOST_CHECK_EQUAL(GD_INDENTATION(), 0);
+
+  {
+    GD_PUSH("C1 indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+    GD_PUSH("C1 indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 2);
+    GD_INFO("C1 => Main");
+    coroutine_switch_to(c1, mc);
+    GD_INFO("C1 <= Main");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 2);
+  }
+
+  BOOST_CHECK_EQUAL(GD_INDENTATION(), 0);
+  GD_INFO_LOG("C1 end => Main");
+  coroutine_switch_to(c1, mc);
+}
+
+static void start_c2(void*)
+{
+  GD_CATEGORY(C2);
+  GD_FILTER_LOG();
+  GD_INFO_LOG("C2 start");
+
+  BOOST_CHECK_EQUAL(GD_INDENTATION(), 0);
+
+  {
+    GD_PUSH("C2 indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 1);
+    GD_PUSH("C2 indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 2);
+    GD_PUSH("C2 indent");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 3);
+    GD_INFO("C2 => C1");
+    coroutine_switch_to(c2, c1);
+    GD_INFO("C2 <= Main");
+    BOOST_CHECK_EQUAL(GD_INDENTATION(), 3);
+    GD_INFO_LOG("C2 end => main");
+    coroutine_switch_to(c2, mc);
+  }
 }
 
 #else
