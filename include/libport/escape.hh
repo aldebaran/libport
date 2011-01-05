2bb5194f7fd24b919e25f1e3596e5c60cd267a0a
Matthieu Nottale <nottale@gostai.com>
2011-01-05 14:43:32 +0100

    Escape: Optimize std::string case by removing intermediate ostream.
    	* include/libport/escape.hh,
    	* include/libport/escape.hxx: Overload ctor for std::string,
    	copy it directly.

--- include/libport/escape.hh
+++ include/libport/escape.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010, Gostai S.A.S.
+ * Copyright (C) 2008-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -32,6 +32,7 @@ namespace libport
   protected:
     template <class T>
       Escape(const T&, char delimiter);
+    Escape(const std::string&, char delimiter);
 
     template <class T>
       friend Escape escape(const T&, char delimiter);
--- include/libport/escape.hxx
+++ include/libport/escape.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010, Gostai S.A.S.
+ * Copyright (C) 2008-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -29,6 +29,13 @@ namespace libport
     std::ostringstream os;
     os << obj;
     str_ = os.str();
+  }
+
+  inline
+  Escape::Escape(const std::string& obj, char delimiter)
+  : str_(obj)
+  , delimiter_(delimiter)
+  {
   }
 
   template <class T>
