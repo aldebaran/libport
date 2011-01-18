f7e60e71fc614e0c91f1fc6bd76da32ba2d24818
Matthieu Nottale <nottale@gostai.com>
2011-01-18 11:52:09 +0100

    coroutine-hook: Fix invalid usage of static std::vector.
    add_coroutine_*_hook can be called at static initialization time.
    So do not rely on a static std::vector that might be initialized
    after.
    
    	* lib/sched/coroutine-hooks.cc: Wrapper function to access
    	the vector.

--- lib/sched/coroutine-hooks.cc
+++ lib/sched/coroutine-hooks.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Gostai S.A.S.
+ * Copyright (C) 2010-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -14,26 +14,34 @@
 
 #include <sched/coroutine-hooks.hh>
 
+/* Warning, add_coroutine_EVENT_hook can be called at static initialization
+ * time, so coroutine_## EVENT ##_hooks cannot be just a static vector.
+ */
 #define COROUTINE_HANDLER(EVENT)                                        \
-static std::vector<coroutine_hook> coroutine_## EVENT ##_hooks;         \
 static void handle_coroutine_## EVENT ##_hooks(Coro *);                 \
                                                                         \
+static std::vector<coroutine_hook>& coroutine_## EVENT ##_hooks()       \
+{                                                                       \
+  static std::vector<coroutine_hook> hook_;                             \
+  return hook_;                                                         \
+}                                                                       \
 void                                                                    \
 add_coroutine_## EVENT ##_hook(coroutine_hook hook)                     \
 {                                                                       \
   if (coroutine_## EVENT ##_hook != &handle_coroutine_## EVENT ##_hooks \
       && coroutine_## EVENT ##_hook != 0)                               \
-    coroutine_## EVENT ##_hooks.push_back(coroutine_## EVENT ##_hook);  \
+    coroutine_## EVENT ##_hooks().push_back(coroutine_## EVENT ##_hook);\
                                                                         \
   coroutine_## EVENT ##_hook = &handle_coroutine_## EVENT ##_hooks;     \
-  coroutine_## EVENT ##_hooks.push_back(hook);                          \
+  coroutine_## EVENT ##_hooks().push_back(hook);                        \
+  std::cerr << coroutine_## EVENT ##_hooks().size() << std::endl;       \
 }                                                                       \
                                                                         \
 static                                                                  \
 void                                                                    \
 handle_coroutine_## EVENT ##_hooks(Coro *c)                             \
 {                                                                       \
-  foreach(coroutine_hook h, coroutine_## EVENT ##_hooks)                \
+  foreach(coroutine_hook h, coroutine_## EVENT ##_hooks())              \
     h(c);                                                               \
 }
 
