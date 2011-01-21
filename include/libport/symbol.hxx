85d6d813a881c16571d734528be6acfc3edeee1a
Quentin Hocquet <hocquet@gostai.com>
2011-01-21 09:59:45 +0100

    Check Symbol do not exceed word size.
    	Symbols must be as small and POD-esque as possible.
    
    	* include/libport/symbol.hxx: Here.

--- include/libport/symbol.hxx
+++ include/libport/symbol.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010, Gostai S.A.S.
+ * Copyright (C) 2008-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -26,6 +26,9 @@
 
 namespace libport
 {
+  // Check Symbol is no more than a POD.
+  BOOST_STATIC_ASSERT(sizeof(Symbol) == sizeof(void*));
+
   //<<
   inline const std::string&
   Symbol::name_get () const
