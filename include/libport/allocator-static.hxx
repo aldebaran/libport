0853447ba03c08b8db3e3e216ca9f348c25a363a
Quentin Hocquet <hocquet@gostai.com>
2011-01-24 18:05:11 +0100

    Static allocator: change forgotten Size in debug mode.
    	* include/libport/allocator-static.hxx: Here.

--- include/libport/allocator-static.hxx
+++ include/libport/allocator-static.hxx
@@ -24,7 +24,7 @@ namespace libport
     (void)size;
 
     // Allocations exceeds the chunk size.
-    assert_le(size / sizeof(long long), Size);
+    assert_le(size / sizeof(long long), Exact::allocator_static_max_size);
 
     if (size_ == storage_size_)
       _grow();
