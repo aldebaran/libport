f8c1f8ac07f3cc4d0dd16848ee1bf00824e6bf88
Quentin Hocquet <hocquet@gostai.com>
2011-03-09 10:29:33 +0100

    Be more carefull with stack overflow on 64 bits.
    	* lib/sched/configuration.cc: Err when 5/6 of the stack where
    	consumed.

--- lib/sched/configuration.cc
+++ lib/sched/configuration.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010, Gostai S.A.S.
+ * Copyright (C) 2009-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -33,6 +33,12 @@ namespace sched
     // Default is large enough until we enable a mechanism to dynamically
     // reallocate task space on demand.
     /* .default_stack_size = */   STACK_SIZE * 1024,
+#if defined(__x86_64__)
+    // 64 bits pointers eat more stack. SEGV have been observed in the
+    // Urbi kernel (2.x/at/reg-trigger-from-condition.log).
+    /* .minimum_stack_size = */   STACK_SIZE * 1024 / 6,
+#else
     /* .minimum_stack_size = */   STACK_SIZE * 1024 / 8,
+#endif
   };
 }
