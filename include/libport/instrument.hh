3383f98965e4093071321319b3376a31f045dbee
Nicolas Pierron <nicolas.b.pierron@gostai.com>
2011-05-10 13:53:14 +0200

    Add Valgrind instrumentation: memory access right, memory validity, pool hooks.
    	* include/libport/instrument.hh: Here.

--- include/libport/instrument.hh
+++ include/libport/instrument.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010, Gostai S.A.S.
+ * Copyright (C) 2009-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -15,10 +15,25 @@
 # include <libport/preproc.hh>
 
 # ifdef LIBPORT_HAVE_VALGRIND_VALGRIND_H
-#  include <valgrind/valgrind.h>
+#  ifndef USE_VALGRIND
+#   define NVALGRIND 1
+#  endif
 # else
 #  define RUNNING_ON_VALGRIND 0
+#  define NVALGRIND 1
+#  ifdef USE_VALGRIND
+#   undef USE_VALGRIND
+#  endif
 # endif
+
+# ifndef NVALGRIND
+#  include <valgrind/valgrind.h>
+#  include <valgrind/memcheck.h>
+# endif
+
+/*-------------.
+| INSTRUMENT.  |
+`-------------*/
 
 # define INSTRUMENTFLAGS(Flags)					\
   const char* LIBPORT_CAT(libport_instrument, __LINE__) =	\
@@ -26,5 +41,42 @@
 
 # define INSTRUMENT_DISABLE()                   \
   INSTRUMENTFLAGS(--mode=none)
+
+/*--------------------------------.
+| Valgrind Memory Access Rights.  |
+`--------------------------------*/
+
+// These macros are used to ensure that no other part of the program can
+// access the variables.  This are means to check for bad memory writes
+// which are made on specific variables.
+# ifndef NVALGRIND
+
+#  define MEM(Right, Addr, Size)                        \
+  LIBPORT_CAT(VALGRIND_MAKE_MEM_, Right) (Addr, Size)
+
+#  define POOL_CREATE(pool, redZone, init)      \
+  VALGRIND_CREATE_MEMPOOL(pool, redZone, init);
+
+#  define POOL_ALLOC(pool, addr, size)          \
+  VALGRIND_MEMPOOL_ALLOC(pool, addr, size);
+
+#  define POOL_FREE(pool, addr)                 \
+  VALGRIND_MEMPOOL_FREE(pool, addr);
+
+# else
+#  define MEM(Right, Addr, Size)
+#  define POOL_CREATE(pool, redZone, init)
+#  define POOL_ALLOC(pool, addr, size)
+#  define POOL_FREE(pool, addr)
+# endif
+
+# define MEM_NOACCESS(Addr, Size)  MEM(NOACCESS, Addr, Size)
+# define MEM_UNDEFINED(Addr, Size) MEM(UNDEFINED, Addr, Size)
+# define MEM_DEFINED(Addr, Size)   MEM(DEFINED, Addr, Size)
+
+# define VAR_NOACCESS(Val)  MEM_NOACCESS(&Val, sizeof(Val))
+# define VAR_UNDEFINED(Val) MEM_UNDEFINED(&Val, sizeof(Val))
+# define VAR_DEFINED(Val)   MEM_DEFINED(&Val, sizeof(Val))
+
 
 #endif
