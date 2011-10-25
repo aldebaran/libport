9b4fbf00c2dabf374aace80a208f9599b899fb50
Akim Demaille <demaille@gostai.com>
2011-10-25 12:10:04 +0200

    ufloat.h: deprecate.
    	* include/libport/ufloat.h: here.

--- include/libport/ufloat.h
+++ include/libport/ufloat.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008, 2010, Gostai S.A.S.
+ * Copyright (C) 2008, 2010-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -10,6 +10,8 @@
 
 #ifndef LIBPORT_UFLOAT_H
 # define LIBPORT_UFLOAT_H
+
+#  warning "<libport/ufloat.h> is deprecated, use <libport/ufloat.hh>."
 
 // Floating point definition (emulated or real)
 # include <libport/ufloat.hh>
