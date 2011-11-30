c0c6682a49d5e75cafc643155d5755d59eecca38
Akim Demaille <demaille@gostai.com>
2011-11-30 11:03:11 +0100

    msvc: fix Libport.Hash failures.
    	* tests/libport/hash.cc: There is no hash function for char*,
    	disable these tests.

--- tests/libport/hash.cc
+++ tests/libport/hash.cc
@@ -9,12 +9,24 @@
  */
 
 #include <libport/bind.hh>
+#include <libport/foreach.hh>
 #include <libport/hash.hh>
 #include <libport/symbol.hh>
 #include <libport/test.hh>
 
 using boost::bind;
 using libport::test_suite;
+
+template <typename K, typename V>
+std::ostream&
+operator<<(std::ostream& o, const boost::unordered_map<K, V>& m)
+{
+  typedef boost::unordered_map<K, V> map_type;
+  typedef map_type::value_type       value_type;
+  foreach (const value_type& p, m)
+    o << p.first << " -> " << p.second << std::endl;
+  return o;
+}
 
 template <typename T>
 void
@@ -26,11 +38,14 @@ test_hash_map()
   BOOST_CHECK_EQUAL(map.size(), 0u);
 
   BOOST_CHECK_NO_THROW(map[T("foo")] = "foo");
+  BOOST_TEST_MESSAGE(map);
   BOOST_CHECK(!map.empty());
   BOOST_CHECK_EQUAL(map.size(), 1u);
   BOOST_CHECK_EQUAL(map[T("foo")], "foo");
+  BOOST_TEST_MESSAGE(map);
 
   BOOST_CHECK_NO_THROW(map[T("bar")] = "bar");
+  BOOST_TEST_MESSAGE(map);
   BOOST_CHECK(!map.empty());
   BOOST_CHECK_EQUAL(map.size(), 2u);
   BOOST_CHECK_EQUAL(map[T("foo")], "foo");
@@ -41,13 +56,16 @@ test_hash_map()
   BOOST_CHECK_EQUAL(map.size(), 2u);
   BOOST_CHECK_EQUAL(map[T("foo")], "foo");
   BOOST_CHECK_EQUAL(map[T("bar")], "baz");
+  BOOST_TEST_MESSAGE(map);
 
   BOOST_CHECK_NO_THROW(map.erase(T("foo")));
+  BOOST_TEST_MESSAGE(map);
   BOOST_CHECK(!map.empty());
   BOOST_CHECK_EQUAL(map.size(), 1u);
   BOOST_CHECK_EQUAL(map[T("bar")], "baz");
 
   BOOST_CHECK_NO_THROW(map.erase(T("bar")));
+  BOOST_TEST_MESSAGE(map);
   BOOST_CHECK(map.empty());
   BOOST_CHECK_EQUAL(map.size(), 0u);
 }
@@ -61,10 +79,19 @@ init_test_suite()
 # define def(Type)                                            \
   suite->add(BOOST_TEST_CASE(&test_hash_map<Type>))
 
-  def(const char*);
-  def(char*);
   def(std::string);
   def(libport::Symbol);
+  // Don't check on char*.
+  //
+  // There is no hash function for char*.  Since the test is using
+  // const strings, GCC is certainly keeping a single copy of them
+  // all, so since "foo" == "foo", of course, the hash table works,
+  // but that's merely "chance".  It turns out that Visual does not
+  // factor the strings (well, that's my understanding), so "of
+  // course" the hash "does not work": it hashes on the addresses, not
+  // the contents.  Since we don't use hashes on char*, let's not
+  // force an interpretation at the library level (after all, we might
+  // want to hash char* like any T*), just don't test char*.
 # undef def
   return suite;
 }
