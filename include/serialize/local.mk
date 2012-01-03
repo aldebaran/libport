a55bb6ffb8752c2e065cff5b3a3417b7c67f6123
Akim Demaille <demaille@gostai.com>
2012-01-03 14:27:07 +0100

    Comment changes.

--- include/serialize/local.mk
+++ include/serialize/local.mk
@@ -1,4 +1,4 @@
-## Copyright (C) 2009-2010, Gostai S.A.S.
+## Copyright (C) 2009-2010, 2012, Gostai S.A.S.
 ##
 ## This software is provided "as is" without warranty of any kind,
 ## either expressed or implied, including but not limited to the
@@ -10,7 +10,7 @@
 serialize_includedir = $(includedir)/serialize
 serialize_include_HEADERS =			\
   include/serialize/binary-i-serializer.hh	\
-  include/serialize/binary-i-serializer.hxx \
+  include/serialize/binary-i-serializer.hxx	\
   include/serialize/binary-o-serializer.hh	\
   include/serialize/binary-o-serializer.hxx	\
   include/serialize/exception.hh		\
@@ -19,5 +19,5 @@ serialize_include_HEADERS =			\
   include/serialize/i-serializer.hh		\
   include/serialize/i-serializer.hxx		\
   include/serialize/o-serializer.hh		\
-  include/serialize/o-serializer.hxx \
+  include/serialize/o-serializer.hxx		\
   include/serialize/serialize.hh
