f0b7ef7f3818b1b5aba41abc6dc5930b497ce16a
Akim Demaille <demaille@gostai.com>
2012-01-24 12:27:47 +0100

    osx: clock_gettime.
    	* include/libport/ctime, lib/libport/ctime.cc (clock_gettime): New.

--- lib/libport/ctime.cc
+++ lib/libport/ctime.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010, Gostai S.A.S.
+ * Copyright (C) 2009-2010, 2012, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -10,10 +10,44 @@
 
 #include <libport/ctime>
 
-# if defined WIN32
+#if defined __APPLE__
+# include <libport/cerrno>
+// https://gist.github.com/1087739
+# include <mach/clock.h>
+# include <mach/mach.h>
 
 int
-clock_gettime(int, struct timespec* t)
+clock_gettime(clockid_t id, timespec* ts)
+{
+  if (id != CLOCK_MONOTONIC
+      && id != CLOCK_REALTIME)
+  {
+    errno = EINVAL;
+    return -1;
+  }
+  if (ts)
+  {
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    // http://lists.apple.com/archives/darwin-development/2002/Nov/msg00092.html
+    clock_id_t cid =
+      id == CLOCK_REALTIME    ? CALENDAR_CLOCK
+      :                         SYSTEM_CLOCK;
+    host_get_clock_service(mach_host_self(), cid, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts->tv_sec = mts.tv_sec;
+    ts->tv_nsec = mts.tv_nsec;
+  }
+  return 0;
+}
+
+#endif
+
+#if defined WIN32
+
+int
+clock_gettime(clockid_t, timespec* t)
 {
   if (t)
   {
@@ -23,8 +57,8 @@ clock_gettime(int, struct timespec* t)
   return 0;
 }
 
-struct tm *
-localtime_r(const time_t *clock, struct tm *res)
+struct tm*
+localtime_r(const time_t* clock, tm* res)
 {
   if (res)
     *res = *localtime(clock);
